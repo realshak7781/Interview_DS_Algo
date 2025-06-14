d1db7a6e413d781f5f2b854c612c03bdc35d07de
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-14 16:17:20 +0530

    Update 220. Contains Duplicate III..cpp

--- HashSet/220. Contains Duplicate III..cpp
+++ HashSet/220. Contains Duplicate III..cpp
@@ -1,4 +1,30 @@
 class Solution {
+private:
+bool optimised(vector<int>& nums, int indexDiff, int valueDiff){
+    int n=nums.size();
+    multiset<long long> ms;
+
+    for(int j=0;j<n;j++){
+        // try to find a n[i]>=n[j]-valueDiff exists or not in the multiset
+
+        auto it=ms.lower_bound(nums[j]-valueDiff);
+
+        if(it!=ms.end() && *it<=(nums[j]+valueDiff)) return true;
+
+        ms.insert(nums[j]);
+
+        // try maintaining a window of size (indexDiff)
+
+        if(j>=indexDiff){
+            auto it=ms.find(nums[j-indexDiff]);
+            if(it!=ms.end()){
+                ms.erase(it);
+            }
+        }
+    }
+
+    return false;
+}    
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
 
@@ -14,5 +40,7 @@ public:
         // }
 
         // return false;
+
+        return optimised(nums,indexDiff,valueDiff);
     }
 };
