2726894d9eb01b3fb9c3f8b3170e65ff8b04d2bd
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-14 10:43:09 +0530

    Update 3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp

--- HashSet/Leetcode Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
+++ HashSet/Leetcode Easy/3396. Minimum Number of Operations to Make Elements in Array Distinct.cpp
@@ -1,29 +1,46 @@
 class Solution {
+private:
+int optimal(vector<int>&nums){
+    int n=nums.size();
+    unordered_set<int> st;
+
+    for(int i=n-1;i>=0;i--){
+        if(st.count(nums[i])){
+            return ceil((i+1)/3.0);
+        }
+        else{
+            st.insert(nums[i]);
+        }
+    }
+    return 0;
+}    
 public:
     int minimumOperations(vector<int>& nums) {
-        int n=nums.size();
+        // int n=nums.size();
 
-        int i=0;
-        int ops=0;
-        while(i<n){
-            bool isDuplicate=false;
-            unordered_set<int> dup;
+        // int i=0;
+        // int ops=0;
+        // while(i<n){
+        //     bool isDuplicate=false;
+        //     unordered_set<int> dup;
 
-            for(int j=i;j<n;j++){
-                if(dup.count(nums[j])){
-                    isDuplicate=true;
-                    break;
-                }else{
-                    dup.insert(nums[j]);
-                }
-            }
+        //     for(int j=i;j<n;j++){
+        //         if(dup.count(nums[j])){
+        //             isDuplicate=true;
+        //             break;
+        //         }else{
+        //             dup.insert(nums[j]);
+        //         }
+        //     }
 
-            if(!isDuplicate) return ops;
+        //     if(!isDuplicate) return ops;
 
-            i+=3;
-            ops++;
-        }
+        //     i+=3;
+        //     ops++;
+        // }
 
-        return ops;
+        // return ops;
+
+        return optimal(nums);
     }
 };
