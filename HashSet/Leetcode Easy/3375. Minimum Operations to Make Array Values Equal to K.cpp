59210f7fb124ef3bee8bfbaa62998501290a05fb
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-14 10:56:43 +0530

    Create 3375. Minimum Operations to Make Array Values Equal to K.cpp

---
+++ HashSet/Leetcode Easy/3375. Minimum Operations to Make Array Values Equal to K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_set<int> st;
+
+        for(int &x:nums){
+            if(x<k){
+                return -1;
+            }
+            else if(x>k){
+                st.insert(x);
+            }
+        }
+
+        return st.size();
+    }
+};
