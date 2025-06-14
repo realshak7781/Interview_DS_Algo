cf97d9213a29f009ccb0c9455ae473972758354f
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-14 17:56:12 +0530

    Update 1684. Count the Number of Consistent Strings.cpp

--- HashSet/1684. Count the Number of Consistent Strings.cpp
+++ HashSet/1684. Count the Number of Consistent Strings.cpp
@@ -1,25 +1,49 @@
 class Solution {
+private:
+int bitMaskApproach(string allowed, vector<string>& words){
+    int allwMask=0;
+    for(char c:allowed){
+        int pos=c-'a';
+        allwMask|=(1<<pos);
+    }
+
+    int count=0;
+    for(string &s:words){
+        int sMask=0;
+
+        for(char c:s){
+            int pos=c-'a';
+            sMask|=(1<<pos);
+        }
+
+        if((sMask|allwMask)==allwMask) count++;
+    }
+
+    return count;
+}    
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        unordered_set<char> allw;
-        for(char c:allowed){
-            allw.insert(c);
-        }
+        // unordered_set<char> allw;
+        // for(char c:allowed){
+        //     allw.insert(c);
+        // }
 
-        int count=0;
-        for(string &s:words){
-            bool isGood=true;
+        // int count=0;
+        // for(string &s:words){
+        //     bool isGood=true;
 
-            for(char c:s){
-                if(!allw.count(c)){
-                    isGood=false;
-                    break;
-                }
-            }
+        //     for(char c:s){
+        //         if(!allw.count(c)){
+        //             isGood=false;
+        //             break;
+        //         }
+        //     }
 
-            if(isGood) count++;
-        }
+        //     if(isGood) count++;
+        // }
 
-        return count;
+        // return count;
+
+        return bitMaskApproach(allowed,words);
     }
 };
