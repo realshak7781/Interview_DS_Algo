49c0880a454b3ce0adbfd3678d967c15b2b9ca63
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-19 19:32:32 +0530

    Update 1425. Constrained Subsequence Sum.cpp

--- Heaps/1425. Constrained Subsequence Sum.cpp
+++ Heaps/1425. Constrained Subsequence Sum.cpp
@@ -1,36 +1,56 @@
-
-//BRUTE FORCE APPROACH : TLE
-
 class Solution {
 private:
-unordered_map<string,int> dp;
-int solve(int prevIdx,int idx,vector<int>&nums,int k){
-    if(idx>=nums.size()){
-        return 0;
-    }
-    string key=to_string(prevIdx)+"#"+"|"+to_string(idx);
-    if(dp.count(key)) return dp[key];
-    // not pick
-    int notPick=0+solve(prevIdx,idx+1,nums,k);
+    unordered_map<string, int> dp;
+    int memo(int prevIdx, int idx, vector<int>& nums, int k) {
+        if (idx >= nums.size()) {
+            return 0;
+        }
+        string key = to_string(prevIdx) + "#" + "|" + to_string(idx);
+        if (dp.count(key))
+            return dp[key];
+        // not pick
+        int notPick = 0 + memo(prevIdx, idx + 1, nums, k);
 
-    int pick=INT_MIN;
-    if(prevIdx==-1 || (idx-prevIdx)<=k){
-        pick=nums[idx]+solve(idx,idx+1,nums,k);
+        int pick = INT_MIN;
+        if (prevIdx == -1 || (idx - prevIdx) <= k) {
+            pick = nums[idx] + memo(idx, idx + 1, nums, k);
+        }
+
+        return dp[key] = max(pick, notPick);
     }
 
-    return dp[key]=max(pick,notPick);
-}    
+    int tab(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 2, 0));
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prevIdx = n; prevIdx >= -1; prevIdx--) {
+                int notPick = dp[idx + 1][prevIdx + 1];
+
+                int pick = INT_MIN;
+                if (prevIdx == -1 || (idx - prevIdx) <= k) {
+                    pick = nums[idx] + dp[idx + 1][idx + 1];
+                }
+
+                dp[idx][prevIdx + 1] = max(pick, notPick);
+            }
+        }
+
+        return dp[0][0]; 
+    }
+
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int maxi=*max_element(nums.begin(),nums.end());
+        int n = nums.size();
+        int maxi = *max_element(nums.begin(), nums.end());
         // all neg case
-        int val=solve(-1,0,nums,k);
+        int val = tab(nums, k);
 
-        if(val==0) return maxi; 
+        if (val == 0)
+            return maxi;
 
-        else return val;
+        else
+            return val;
     }
 };
-
-
