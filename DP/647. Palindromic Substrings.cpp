af0c4a8158d394759e6649d8c90926a779a11a75
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-22 16:13:55 +0530

    Update 647. Palindromic Substrings.cpp

--- DP/647. Palindromic Substrings.cpp
+++ DP/647. Palindromic Substrings.cpp
@@ -1,3 +1,34 @@
+class Solution {
+private:
+vector<vector<int>> dp;
+bool checkPalindrome(string &s,int i,int j){
+    if(i>j) return true;
+
+    if(dp[i][j]!=-1) return dp[i][j];
+    bool ans=false;
+    if(s[i]==s[j]){
+        ans=checkPalindrome(s,i+1,j-1);
+    }
+
+    return dp[i][j]=ans;
+}    
+public:
+    int countSubstrings(string s) {
+        int n=s.length();
+        
+        dp=vector<vector<int>>(1001,vector<int>(n+1,-1));
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(checkPalindrome(s,i,j)) count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+
 
 //brute force o(n^3)
 class Solution {
