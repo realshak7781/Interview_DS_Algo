68849d12bdfd1955e9b5531f350b3fd8e8b58091
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-23 09:05:50 +0530

    Update 1218. Longest Arithmetic Subsequence of Given Difference.cpp

--- DP/1-D DP/1218. Longest Arithmetic Subsequence of Given Difference.cpp
+++ DP/1-D DP/1218. Longest Arithmetic Subsequence of Given Difference.cpp
@@ -4,6 +4,40 @@ public:
         int n=arr.size();
 
         // bottom up approach
+        unordered_map<int,int> dp;
+        int maxLen=0;
+        for(int i=0;i<n;i++){
+            // for(int j=i-1;j>=0;j--){
+            //     if((arr[i]-arr[j])==difference){
+            //         dp[i]=max(dp[i],dp[j]+1);
+            //     }
+            // }
+
+            int prev=arr[i]-difference;
+            int cur=arr[i];
+
+            if(dp.count(prev)){
+                dp[cur]=max(dp[cur],dp[prev]+1);
+            }
+            else{
+                dp[cur]=1;
+            }
+
+            maxLen=max(maxLen,dp[cur]);
+        }
+
+        return maxLen;
+    }
+};
+
+
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) {
+        int n=arr.size();
+
+        // bottom up approach
 
         vector<int> dp(n,1);
 
