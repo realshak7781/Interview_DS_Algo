b528200c044d601d15d4e19d5928b7715b350aca
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-23 17:37:44 +0530

    Update 1911. Maximum Alternating Subsequence Sum.cpp

--- DP/1-D DP/1911. Maximum Alternating Subsequence Sum.cpp
+++ DP/1-D DP/1911. Maximum Alternating Subsequence Sum.cpp
@@ -1,3 +1,42 @@
+// BOTTOM UP APPROACH:
+using ll = long long;
+class Solution {
+private:
+
+    ll bottomUp(vector<int>& nums) {
+        int n = nums.size();
+
+        vector < vector<ll>> dp(n+1, vector<ll>(2, 0));
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int parity = 0; parity<= 1; parity++) {
+                ll notPick = 0 + dp[idx+1][parity];
+                // parity-->0:even and parity--> 1: odd
+                ll pick = 0;
+                if (parity == 0) {
+                    pick =   dp[idx+1][1-parity]+ nums[idx];
+                } else {
+                    pick =  dp[idx+1][1-parity]- nums[idx];
+                }
+                dp[idx][parity] = max(pick, notPick);
+            }
+
+        }
+
+        return max(dp[0][1],dp[0][0]);
+    }
+
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        return bottomUp(nums);
+    }
+};
+
+
+
+
+//RECURSIVE AND MEMO APPROACH
 using ll=long long;
 class Solution {
 private:
