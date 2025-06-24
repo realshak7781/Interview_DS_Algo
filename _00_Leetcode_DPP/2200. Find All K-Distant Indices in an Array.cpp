6133f129be1cc04800c66775769fa21078b7e8ea
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-24 12:12:42 +0530

    Update 2200. Find All K-Distant Indices in an Array.cpp

--- _00_Leetcode_DPP/2200. Find All K-Distant Indices in an Array.cpp
+++ _00_Leetcode_DPP/2200. Find All K-Distant Indices in an Array.cpp
@@ -1,4 +1,38 @@
+// optimal:
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        int n=nums.size();
+        vector<int> res;
 
+        vector<int> vis(n,false);
+        for(int i=0;i<n;i++){
+            if(nums[i]!=key) continue;
+
+            // expand lefft and right
+            if(!vis[i])res.push_back(i);
+            int j=i-1;
+            int m=i+1;
+
+            while(j>=0 && (i-j)<=k){
+                if(!vis[j])res.push_back(j);
+                vis[j]=true;
+                j--;
+            }
+
+            while(m<n && (m-i)<=k){
+                if(!vis[m])res.push_back(m);
+                vis[m]=true;
+                m++;
+            }
+            vis[i]=true;
+        }
+
+        sort(res.begin(),res.end());
+
+        return res;
+    }
+};
 
 //BRUTE FORCE SOLUTION:
 class Solution {
