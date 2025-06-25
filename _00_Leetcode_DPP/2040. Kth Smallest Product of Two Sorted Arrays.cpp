1952fa843ecfde0d67a52f70caabb8dcb7091b99
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-25 13:52:13 +0530

    Update 2040. Kth Smallest Product of Two Sorted Arrays.cpp

--- _00_Leetcode_DPP/2040. Kth Smallest Product of Two Sorted Arrays.cpp
+++ _00_Leetcode_DPP/2040. Kth Smallest Product of Two Sorted Arrays.cpp
@@ -1,4 +1,80 @@
+// T.C: O(LOG(RANGE)*N*LOG(M))
+// S.C: O(1)
 
+
+using ll = long long;
+class Solution {
+public:
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2,
+                                 long long k) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        // we need to make use of the sorted nature of the arrays
+        //  we are not making the use of that in the heap approach
+        // using the concept of binary search on answers
+        ll low = -1e10, high = 1e10;
+
+        ll res;
+        while (low <= high) {
+            ll mid = low + (high - low) / 2;
+            ll count = getCount(nums1, nums2, k, mid);
+
+            if (count>=k) {
+               high=mid-1;
+               res=mid;
+            }
+            else{
+                low=mid+1;
+            }
+           
+        }
+
+        return res;
+    }
+
+private:
+    ll getCount(vector<int>& nums1, vector<int>& nums2, long long k,
+                ll reqProd) {
+        ll totalCount = 0;
+
+        for (int& num : nums1) {
+            if (num >= 0) {
+                ll left = 0, right = nums2.size() - 1;
+
+                while (left <= right) {
+                    ll mid = left + (right - left) / 2;
+                    ll prod = 1LL * nums2[mid] * num;
+
+                    if (prod <= reqProd) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+
+                totalCount += left;
+            } else {
+                ll left = 0, right = nums2.size() - 1;
+
+                while (left <= right) {
+                    ll mid = left + (right - left) / 2;
+                    ll prod = 1LL * nums2[mid] * num;
+
+                    if (prod <= reqProd) {
+                       right=mid-1;
+                    } else {
+                        left = mid +1;
+                    }
+                }
+
+                totalCount += (nums2.size()-left);
+            }
+        }
+
+        return totalCount;
+    }
+};
 
 
 // BRUTE FORCE APPROACH : TLE
