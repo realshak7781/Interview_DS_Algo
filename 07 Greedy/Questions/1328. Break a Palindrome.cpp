dcb50e1231c31cb30ea366e55bb05ba8d7d9dbb7
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-26 20:04:10 +0530

    Update 1328. Break a Palindrome.cpp

--- 07 Greedy/Questions/1328. Break a Palindrome.cpp
+++ 07 Greedy/Questions/1328. Break a Palindrome.cpp
@@ -1,4 +1,23 @@
+// OPTIMAL :  O(N)
 
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n=palindrome.length();
+        if(n==1) return "";
+        
+        for(int i=0;i<n/2;i++){
+            if(palindrome[i]!='a'){
+                palindrome[i]='a';
+                return palindrome;
+            }
+        }
+
+        palindrome[n-1]='b';
+
+        return palindrome;
+    }
+};
 
 
 //  BRUTE FORCE ; O(N*26*N)
