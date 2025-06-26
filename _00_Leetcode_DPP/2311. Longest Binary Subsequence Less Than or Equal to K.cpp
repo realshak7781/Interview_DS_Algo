4c92f0192e510e9b10a8cd34f07b31a45df37e82
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-26 12:32:05 +0530

    Update 2311. Longest Binary Subsequence Less Than or Equal to K.cpp

--- _00_Leetcode_DPP/2311. Longest Binary Subsequence Less Than or Equal to K.cpp
+++ _00_Leetcode_DPP/2311. Longest Binary Subsequence Less Than or Equal to K.cpp
@@ -1,4 +1,38 @@
 
+// GREEDY APPROACH : O(N)
+using ll=long long;
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        int n = s.length();
+
+        string res="";
+        for(int i=n-1;i>=0;i--){
+
+            if(s[i]=='0'){
+                // adding zerroes to the front wont change value so increase he length
+                res=s[i]+res;
+                // adding cur char to the front cause for loop is reversed
+            }
+            else{
+                // but add 1 only if it makes the value <=k;
+                string org=res;
+                res=s[i]+res;
+                bitset<64> bt(res);
+
+                if(bt.to_ulong()<=k)continue;
+
+                res=org;
+            }
+        }
+
+        return res.size();
+    }
+};
+
+
+
+
 // RECURSION + MEMO : TLE
 using ll=long long;
 class Solution {
