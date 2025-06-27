f545fb430c6d49f183c1e70f5a823b76c16f796d
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-27 16:01:56 +0530

    Update 991. Broken Calculator.cpp

--- 07 Greedy/Questions/991. Broken Calculator.cpp
+++ 07 Greedy/Questions/991. Broken Calculator.cpp
@@ -1,4 +1,37 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        // the question states that : " multiply the number on display by 2"
+        // and the constraints are high : clearly they want you to implement a log base 2 solution
+        
 
+//         NOTE:
+//         If during interview someone asks you why did you choose to work backward, you can give this solution
+
+
+// When working forward from X to Y, at each step we face a decision problem: should we multiply or subtract? This creates a huge number of possible paths.
+// But when working backwards from Y to X, there's always a clear best choice:
+
+// If Y is odd, we must have added 1 to an even number (since multiplying by 2 always gives an even result)
+// If Y is even, dividing by 2 is always better than adding 1 multiple times
+
+// This approach eliminates any ambiguity about which operation to choose at each step, and it's guaranteed to give us the minimum number of operations
+
+
+        int ops=0;
+         while(target!=startValue){
+            if(target%2==0 && target>startValue){
+                target/=2;
+            }
+            else{
+                target+=1;
+            }
+            ops++;
+         }
+
+         return ops;
+    }
+};
 
 
 // BFS APPROACH : TLE
