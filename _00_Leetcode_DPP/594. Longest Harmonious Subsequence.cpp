ad6cfdd71d64606bb31f3198a2104db8912e500d
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-06-30 12:20:56 +0530

    Update 594. Longest Harmonious Subsequence.cpp

--- _00_Leetcode_DPP/594. Longest Harmonious Subsequence.cpp
+++ _00_Leetcode_DPP/594. Longest Harmonious Subsequence.cpp
@@ -1,4 +1,30 @@
+// BETTER : O(NLOGN)
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
 
+        unordered_map<int,int> occ;
+        int maxLen=0;
+        for(int i=0;i<n;i++){
+            // we want max -min=1
+            // take the cur element as max then min=max-1;
+            int mini=nums[i]-1;
+            // try to find does mini exist in the map
+            if(occ.count(mini)){
+                int len=i-occ[mini]+1;
+                maxLen=max(maxLen,len);
+            }
+
+            // put the current nums[i] into map but if it already exists dont 
+
+            if(!occ.count(nums[i])) occ[nums[i]]=i;
+        }
+
+        return maxLen;
+    }
+};
 
 // BRUTE FORCE: O(N^2) +O(NLOGN)
 class Solution {
