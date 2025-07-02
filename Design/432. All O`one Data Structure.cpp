dfcc0356ce1390e04a8ed9930cc794895e7c91c0
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-02 16:45:09 +0530

    Update 432. All O`one Data Structure.cpp

--- Design/432. All O`one Data Structure.cpp
+++ Design/432. All O`one Data Structure.cpp
@@ -1,3 +1,57 @@
+// Better:
+
+struct cmp {
+    bool operator()(const pair<string, int>& a, const pair<string, int>& b) const {
+        if (a.second == b.second)
+            return a.first < b.first;
+        return a.second < b.second;
+    }
+};
+
+class AllOne {
+private:
+    unordered_map<string, int> mp;
+    multiset<pair<string, int>, cmp> ms;
+
+public:
+    AllOne() {}
+
+    void inc(string key) {
+        if (mp.count(key)) {
+            int oldCnt = mp[key];
+            ms.erase(ms.find({key, oldCnt}));
+            mp[key]++;
+            ms.insert({key, mp[key]});
+        } else {
+            mp[key] = 1;
+            ms.insert({key, 1});
+        }
+    }
+
+    void dec(string key) {
+        if (!mp.count(key)) return;
+
+        int oldCnt = mp[key];
+        ms.erase(ms.find({key, oldCnt}));
+
+        mp[key]--;
+        if (mp[key] == 0) {
+            mp.erase(key);
+        } else {
+            ms.insert({key, mp[key]});
+        }
+    }
+
+    string getMaxKey() {
+        if (ms.empty()) return "";
+        return ms.rbegin()->first;
+    }
+
+    string getMinKey() {
+        if (ms.empty()) return "";
+        return ms.begin()->first;
+    }
+};
 
 
 
