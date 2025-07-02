7b244ed819008485cc9bf80b8704371b7038f3ea
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-02 18:07:21 +0530

    Update 432. All O`one Data Structure.cpp

--- Design/432. All O`one Data Structure.cpp
+++ Design/432. All O`one Data Structure.cpp
@@ -1,3 +1,128 @@
+// optimal: map + dll
+
+struct Node{
+    int count;
+    unordered_set<string> keys;
+    Node* next;
+    Node* prev;
+    Node(int c){
+        count=c;
+        next=nullptr;
+        prev=nullptr;
+    }
+};
+
+class AllOne {
+private:
+unordered_map<string,Node*> mp;
+Node*head;
+Node*last;    
+public:
+    AllOne() {
+        head=new Node(0);
+        last=head;
+    }
+
+    void addNode(Node*prevNode,int cnt){
+        Node *newNode=new Node(cnt);
+        newNode->next=prevNode->next;
+        newNode->prev=prevNode;
+
+        if(prevNode->next!=nullptr){
+            newNode->next->prev=newNode;
+        }
+        prevNode->next=newNode;
+
+        if(prevNode==last){
+            last=newNode;
+        }
+    }
+
+    void removeNode(Node*node){
+        node->prev->next=node->next;
+
+        if(node->next!=nullptr){
+            node->next->prev=node->prev;
+        }
+
+        if(last==node){
+            last=node->prev;
+        }
+
+        delete node;
+    }
+    
+    void inc(string key) {
+        if(!mp.count(key)){
+            // key doesnt exist --> first entry
+            // check if there exists a node with count=1;
+
+            if(head->next==nullptr || head->next->count!=1){
+                addNode(head,1);
+            }
+            head->next->keys.insert(key);
+            mp[key]=(head->next);
+        }
+        else{
+            // key exists with some count
+            Node *curNode=mp[key];
+            int curCnt=curNode->count;
+
+            if(curNode->next==nullptr || curNode->next->count!=curCnt+1){
+                // means 2->5 , make it 2->3->5
+                addNode(curNode,curCnt+1);
+            }
+
+            curNode->next->keys.insert(key);
+            mp[key]=curNode->next;
+            curNode->keys.erase(key);
+
+            if(curNode->keys.empty()){
+                removeNode(curNode);
+            }
+        }
+    }
+    
+    void dec(string key) {
+        Node*curNode=mp[key];
+        int curCnt=curNode->count;
+
+        if(curCnt==1){
+            mp.erase(key);
+        }else{
+            if(curNode->prev->count!=(curCnt-1)){
+                addNode(curNode->prev,curCnt-1);
+            }
+
+            curNode->prev->keys.insert(key);
+            mp[key]=curNode->prev;
+        }
+
+        curNode->keys.erase(key);
+        if(curNode->keys.empty()){
+            removeNode(curNode);
+        }
+    }
+    
+    string getMaxKey() {
+        if(last==head){
+            return "";
+        }
+
+        return *(last->keys.begin());
+    }
+    
+    string getMinKey() {
+        if(head->next==nullptr) return "";
+
+        return *(head->next->keys.begin());
+    }
+};
+
+
+
+
+
 // Better:
 
 struct cmp {
