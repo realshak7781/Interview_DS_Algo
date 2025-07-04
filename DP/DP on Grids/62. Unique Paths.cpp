8f5b0da7d9495d56d1742bd35c1d463536f098bc
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-04 18:00:24 +0530

    Update 62. Unique Paths.cpp

--- DP/DP on Grids/62. Unique Paths.cpp
+++ DP/DP on Grids/62. Unique Paths.cpp
@@ -1,3 +1,46 @@
+
+
+
+
+
+
+
+
+// APPROACH 2 : BOTTOM UP
+class Solution {
+private:
+
+
+int bottomUp(int m,int n){
+    vector<vector<int>> dp(m,vector<int>(n,0));
+    dp[m-1][n-1]=1;
+
+    for(int i=m-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
+             if (i == m-1 && j == n-1) continue;
+            int down=0,right=0;
+            if(i+1<m){
+                down=dp[i+1][j];
+            }
+            if(j+1<n){
+                right=dp[i][j+1];
+            }
+
+            dp[i][j]=down+right;
+        }
+    }
+
+    return dp[0][0];
+}
+
+public:
+    int uniquePaths(int m, int n) {
+        return bottomUp(m,n);
+    }
+};
+
+
+
 // APPROACH 1 : RECURSION + MEMO
 class Solution {
 private:
