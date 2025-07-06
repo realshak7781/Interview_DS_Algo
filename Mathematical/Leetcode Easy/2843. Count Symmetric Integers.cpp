57279fbe3551021e09bc35c1daf06abb15c70029
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-06 21:50:21 +0530

    Update 2843. Count Symmetric Integers.cpp

--- Mathematical/Leetcode Easy/2843. Count Symmetric Integers.cpp
+++ Mathematical/Leetcode Easy/2843. Count Symmetric Integers.cpp
@@ -31,3 +31,36 @@ public:
         return frontSum==endSum;
     }
 };
+
+
+
+// APPROACH 2 : MATHEMATICAL
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        // as per constraints 
+        //  we can form only two len sym numbers and 4 len sym numbers;
+
+        int symCnt=0;
+        for(int num=low;num<=high;num++){
+            if(num>10 && num<=99 && num%11==0){
+                symCnt++;
+            }
+            else if(num>1000){
+                // 4 digit numbers : check separately
+                int first=num/1000;
+                int second=(num/100)%10;
+                int startSum=first+second;
+
+                int third=(num/10)%10;
+                int fourth=num%10;
+
+                int endSum=third+fourth;
+
+                if(startSum==endSum) symCnt++;
+            }
+        }
+
+        return symCnt;
+    }
+};
