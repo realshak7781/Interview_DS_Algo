d987bdfb570a4520836bd7e347ea17b84ae938bb
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-06 19:51:11 +0530

    Update 174. Dungeon Game.cpp

--- DP/DP on Grids/174. Dungeon Game.cpp
+++ DP/DP on Grids/174. Dungeon Game.cpp
@@ -1,3 +1,52 @@
+// BOTTOM UP:
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        // state definition for bottom up approach'
+        //  dp[i][j]=minHealth required to reach from (i,j) to (m,n)
+        // dp[0][0]=minHealth required to reach from (0,0) to (m,n)
+
+        int m=dungeon.size();
+        int n=dungeon[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,INT_MAX));
+
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1){
+                    dp[i][j]= dungeon[i][j]>=0?1:(1-dungeon[i][j]);
+                }
+                else{
+                    int down=INT_MAX;
+                    if(i+1<m){
+                        down=dp[i+1][j];
+                    }
+
+                    int right=INT_MAX;
+                    if(j+1<n){
+                        right=dp[i][j+1];
+                    }
+
+                    int need=min(down,right);
+
+                    if(dungeon[i][j]>=need){
+                        need=1;
+                    }
+                    else{
+                        need-=dungeon[i][j];
+                    }
+
+                    dp[i][j]=need;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
+
+// RECURSION + MEMO
 class Solution {
 private:
 int m,n;  
