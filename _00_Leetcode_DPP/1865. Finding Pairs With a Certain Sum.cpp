08286d13e62b22e11d52c5f52daaba9a9253f3c6
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-06 18:02:15 +0530

    Create 1865. Finding Pairs With a Certain Sum.cpp

---
+++ _00_Leetcode_DPP/1865. Finding Pairs With a Certain Sum.cpp
@@ -0,0 +1,39 @@
+
+
+
+//BRUTE FORCE : SORTING + BINARY SEARCH
+class FindSumPairs {
+vector<int>nums1,nums2;
+public:
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        this->nums1=nums1;
+        this->nums2=nums2;
+    }
+    
+    void add(int index, int val) {
+        if(index>=nums2.size()) return;
+
+        nums2[index]+=val;
+    }
+    
+    int count(int tot) {
+        int cnt=0;
+        for(int i=0;i<nums1.size();i++){
+            int n2=tot-nums1[i];
+            cnt+=(getCount(nums2,n2));
+        }
+
+        return cnt;
+    }
+
+
+    int getCount(vector<int> nums2,int n2){
+        sort(nums2.begin(),nums2.end());
+        int lb=lower_bound(nums2.begin(),nums2.end(),n2)-nums2.begin();
+        int ub=upper_bound(nums2.begin(),nums2.end(),n2)-nums2.begin();
+
+        int count=ub-lb;
+        return count;
+    }
+};
+
