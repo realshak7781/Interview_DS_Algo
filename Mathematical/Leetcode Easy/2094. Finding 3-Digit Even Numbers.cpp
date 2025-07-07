976cb8ef51ccb7c47cc769654675a24463d8537e
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-07 14:02:14 +0530

    Update 2094. Finding 3-Digit Even Numbers.cpp

--- Mathematical/Leetcode Easy/2094. Finding 3-Digit Even Numbers.cpp
+++ Mathematical/Leetcode Easy/2094. Finding 3-Digit Even Numbers.cpp
@@ -1,4 +1,40 @@
 
+// MATHEMATICAL
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        unordered_map<int,int> mp;
+
+        for(int dig:digits) mp[dig]++;
+
+        vector<int> res;
+
+        for(int i=1;i<=9;i++){
+            if(mp[i]==0) continue;
+            mp[i]--;
+
+            for(int j=0;j<=9;j++){
+                if(mp[j]==0) continue;
+                mp[j]--;
+
+                for(int k=0;k<=8;k+=2){
+                    if(mp[k]==0) continue;
+                    mp[k]--;
+
+                    int num=i*100 +j*10+k;
+                    res.push_back(num);
+
+                    mp[k]++;
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+
+
+        return res;
+    }
+};
 
 // BRUTE FORCE APPROACH:
 class Solution {
