42e59ac16cf5c1f491081dac6d98b6beb1393b53
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-07 23:30:10 +0530

    Update 2582. Pass the Pillow.cpp

--- Mathematical/Leetcode Easy/2582. Pass the Pillow.cpp
+++ Mathematical/Leetcode Easy/2582. Pass the Pillow.cpp
@@ -1,4 +1,30 @@
 
+// MATHEMATICAL :O(1) APPROACH
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        // TO REACH N WE NEED N-1 SECONDS
+
+        int res=1;
+        int tripTime=n-1;
+
+        if(time<=tripTime){
+            res+=time;
+        }
+        else{
+            int trips=time/tripTime;
+            int rem=time%tripTime;
+            if(trips&1){
+                res=n-rem;
+            }
+            else{
+                res+=rem;
+            }
+        }
+
+        return res;
+    }
+};
 
 // BRUTE FORCE : O(T)
 class Solution {
