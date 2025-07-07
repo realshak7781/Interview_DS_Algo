87bc4e3a6eee130d1940a79e8b11d2a3dd103dc3
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-07 17:26:17 +0530

    Update 1353. Maximum Number of Events That Can Be Attended.cpp

--- _00_Leetcode_DPP/1353. Maximum Number of Events That Can Be Attended.cpp
+++ _00_Leetcode_DPP/1353. Maximum Number of Events That Can Be Attended.cpp
@@ -1,4 +1,35 @@
+// optimal:
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        int n=events.size();
 
+        sort(events.begin(),events.end());
+        priority_queue<int,vector<int>,greater<int>> pq;
+        int day=events[0][0];
+        int res=0;
+
+        int i=0;
+        while(i<n || !pq.empty()){
+
+            while(i<n && events[i][0]==day){
+                pq.push(events[i][1]);
+                i++;
+            }
+            // remove events whose last day to be attended has passed
+            while(!pq.empty() && pq.top()<day) pq.pop();
+
+            if(!pq.empty()){
+                pq.pop();
+                res++;
+            }
+
+            day++;
+        }
+
+        return res;
+    }
+};
 
 
 // brute force:
