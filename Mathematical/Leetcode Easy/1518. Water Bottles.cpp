9ec30f2c1fcc8043718d814d652da84c27eabc88
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-08 12:12:42 +0530

    Update 1518. Water Bottles.cpp

--- Mathematical/Leetcode Easy/1518. Water Bottles.cpp
+++ Mathematical/Leetcode Easy/1518. Water Bottles.cpp
@@ -1,3 +1,22 @@
+// Optimal:
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int consumed=numBottles;
+        int emptyBottles=numBottles;
+
+        while(emptyBottles>=numExchange){
+            int newBottles=emptyBottles/numExchange;
+            int remEmpty=emptyBottles%numExchange;
+
+            consumed+=newBottles;
+            emptyBottles=newBottles+remEmpty;
+        }
+
+        return consumed;
+    }
+};
 
 // simple simulation:
 class Solution {
