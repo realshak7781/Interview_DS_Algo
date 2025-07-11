4dc73c0a608ab79a9066206709e1636adea1b03c
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-11 21:02:29 +0530

    Update 1598. Crawler Log Folder.cpp

--- Stack/LeetCode Easy/1598. Crawler Log Folder.cpp
+++ Stack/LeetCode Easy/1598. Crawler Log Folder.cpp
@@ -1,4 +1,36 @@
+// optimal:
+// without : vectors or strings:
 
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int count=0;
+        int n=logs.size();
+        for(int i=0;i<n;i++){
+            string cur=logs[i];
+
+            if(cur=="../"){
+                // remove the last sub folder if it exists
+                if(count==0){
+                    continue;
+                }
+                else{
+                    count--;
+                }
+            }
+            else if(cur=="./"){
+                // do nothing
+                continue;
+            }
+            else{
+                // put into folders; 
+               count++;
+            }
+        }
+
+        return count;
+    }
+};
 
 // brute force: O(n) + space : o(n)
 class Solution {
