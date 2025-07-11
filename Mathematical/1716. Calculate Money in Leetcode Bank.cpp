7beb5c33ba35f0f48f4ee232a655924c5d84854d
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-11 19:30:10 +0530

    Update 1716. Calculate Money in Leetcode Bank.cpp

--- Mathematical/1716. Calculate Money in Leetcode Bank.cpp
+++ Mathematical/1716. Calculate Money in Leetcode Bank.cpp
@@ -1,4 +1,31 @@
 
+// O(6)~ o(1)
+class Solution {
+public:
+    int totalMoney(int n) {
+       if(n<=7){
+         return n*(n+1)/2;
+       }
+
+       int items=n/7;
+       int first=28;
+       int last=28+(items-1)*7;
+    //     the days form and AP: 28 ,35, and so on
+       int arithmeticSum= items*(first+last)/2;
+
+       int remDays=n%7;
+
+       int mondayPrice=items+1;
+       int finalRemMoney=0;
+
+       for(int day=1;day<=remDays;day++){
+            finalRemMoney+=mondayPrice;
+            mondayPrice++;
+       }
+
+       return finalRemMoney+arithmeticSum;
+    }
+};
 // BRUTE FORCE: O(N)
 class Solution {
 public:
