45be236d56c144eb4c43b32eb2fbe79fcb0567c7
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-11 15:38:58 +0530

    Create 319. Bulb Switcher.cpp

---
+++ Mathematical/319. Bulb Switcher.cpp
@@ -0,0 +1,28 @@
+
+
+
+// T.C: O(N)
+// S.C : O(N)
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        if(n==0 || n==1) return n;
+
+       vector<int> bulbs(n+1,1);
+
+       for(int i=2;i<=n;i++){
+        for(int j=i;j<=n;j+=i){
+            bulbs[j]=1-bulbs[j];
+        }
+       }
+
+       int count=0;
+
+       for(int i=1;i<=n;i++){
+        if(bulbs[i]==1) count++;
+       }
+
+
+       return count;
+    }
+};
