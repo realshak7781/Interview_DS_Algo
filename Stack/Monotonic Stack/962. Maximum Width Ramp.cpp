d7affde63726c7c678e87f159426dda15c6c32ed
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-12 18:42:19 +0530

    Update and rename Stack/962. Maximum Width Ramp.cpp to Stack/Monotonic Stack/962. Maximum Width Ramp.cpp

--- Stack/Monotonic Stack/962. Maximum Width Ramp.cpp
+++ Stack/Monotonic Stack/962. Maximum Width Ramp.cpp
@@ -1,3 +1,30 @@
+// optimal:
+
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> maxRight(n);
+        maxRight[n-1]=nums[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            maxRight[i]=max(maxRight[i+1],nums[i]);
+        }
+
+        int ramp=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            while(j<n && maxRight[j]>=nums[i]){
+                ramp=max(ramp,j-i);
+                j++;
+            }
+
+            if(j==n) return ramp;
+        }
+
+        return ramp;
+    }
+};
 
 // Better : O(n^2)
 class Solution {
