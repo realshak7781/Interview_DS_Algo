2939727863e0f5d5a9f0b46956e9b4640d6a71ab
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-13 21:43:37 +0530

    Update 735. Asteroid Collision.cpp

--- Stack/735. Asteroid Collision.cpp
+++ Stack/735. Asteroid Collision.cpp
@@ -1,4 +1,43 @@
 
+// stack based approach : O(n) time and O(n) space
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        int n=asteroids.size();
+        for(int i=0;i<n;i++){
+            int cur=asteroids[i];
+            bool destroyed=false;
+            while(!st.empty() && st.top()>0 && cur<0){
+                if(st.top()==abs(cur)){
+                    st.pop();
+                    destroyed=true;
+                    break;
+                }
+                else if(st.top()> abs(cur)){
+                    destroyed=true;
+                    break;
+                }
+                else{
+                    st.pop();
+                }
+            }
+
+            if(!destroyed) st.push(cur);
+        }
+
+
+        vector<int> res;
+        while(!st.empty()){
+            res.push_back(st.top());st.pop();
+        }
+
+        reverse(res.begin(),res.end());
+
+
+        return res;
+    }
+};
 
 // vector<based > approach :O(n) and space : O(N)
 class Solution {
