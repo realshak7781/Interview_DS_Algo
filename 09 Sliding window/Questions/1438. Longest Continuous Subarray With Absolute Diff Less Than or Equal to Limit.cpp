147298f4694a18aedecf8e49af1a2e558399f492
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-14 23:27:07 +0530

    Update 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

--- 09 Sliding window/Questions/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ 09 Sliding window/Questions/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -1,8 +1,11 @@
+//  time :O(nlogn)
+// space : O(n)
 using p=pair<int,int>;
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
         multiset<p> ms;
+        // O(n) space
         int start=0;
         int n=nums.size();
 
@@ -11,11 +14,13 @@ public:
         for(int end=0;end<n;end++){
 
             ms.insert({nums[end],end});
+            // o(logn) time
 
             int mini=ms.begin()->first;
             int maxi=ms.rbegin()->first;
             while(!ms.empty() && maxi-mini>limit){
                 ms.erase({nums[start],start});
+                // O(logn) time
                 start++;
 
                 if(!ms.empty()){
