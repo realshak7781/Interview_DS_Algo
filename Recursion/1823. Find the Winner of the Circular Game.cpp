6bac01a7ee155d556fba84345e4bd2c3defb5e05
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-16 19:33:42 +0530

    Update 1823. Find the Winner of the Circular Game.cpp

--- Recursion/1823. Find the Winner of the Circular Game.cpp
+++ Recursion/1823. Find the Winner of the Circular Game.cpp
@@ -1,4 +1,25 @@
+// queue based approacch:
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        // the goal is : the game starts at 1 and eliminates k-1th guy from start , so our goal should be to be: the guy to remain alive till last and not be eliminated : so think in terms of the position you would choose to not get eliminated:
 
+        // queue based approacch:
+        queue<int> q;
+        for(int i=1;i<=n;i++)q.push(i);
+
+        while(q.size()>1){
+
+            for(int cnt=1;cnt<k;cnt++){
+                q.push(q.front());
+                q.pop();
+            }
+            q.pop();
+        }
+
+        return q.front();
+    }
+};
 
 // brute force approach :
 // time: O(nk + nlogn)
