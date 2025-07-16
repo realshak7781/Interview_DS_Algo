012d2eeeddd92969600a5f71907e8a076e400541
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-16 09:48:28 +0530

    Update 1545. Find Kth Bit in Nth Binary String.cpp

--- Recursion/1545. Find Kth Bit in Nth Binary String.cpp
+++ Recursion/1545. Find Kth Bit in Nth Binary String.cpp
@@ -1,3 +1,39 @@
+// optimal: O(N)time and O(N) space
+
+class Solution {
+private:
+int findPower(int a,int b){
+    if(b==0) return 1;
+    int half=findPower(a,b/2);
+    int result=half*half;
+    if(b&1){
+        result=a*result;
+    }
+
+    return result;
+}
+public:
+    char findKthBit(int n, int k) {
+         if(n == 1) {
+            return '0';
+        }
+        int len=findPower(2,n)-1;
+
+        if(k<ceil(len/2.0)){
+            return findKthBit(n-1,k);
+        }
+        else if(k==ceil(len/2.0)){
+            return '1';
+        }
+        else{
+            char c=findKthBit(n-1,len-(k-1));
+            return c=='1'?'0':'1';
+        }
+
+        return '/0';
+    }
+};
+
 
 // brute force : o(2^N)
 class Solution {
