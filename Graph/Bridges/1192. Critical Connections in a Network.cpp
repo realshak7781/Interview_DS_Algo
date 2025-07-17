cbbf331f7aa2968076988d3f772ee15f3009cd75
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-17 22:51:41 +0530

    Create 1192. Critical Connections in a Network.cpp

---
+++ Graph/Bridges/1192. Critical Connections in a Network.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        // its a  basic implementation of tarjans algorithm
+        
+    }
+};
