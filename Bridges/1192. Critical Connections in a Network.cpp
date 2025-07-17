e3786c076ac4a07d0182d3daaccf2cf67131c435
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-17 22:50:58 +0530

    Delete Bridges directory

--- Bridges/1192. Critical Connections in a Network.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        // its a  basic implementation of tarjans algorithm
-        
-    }
-};
