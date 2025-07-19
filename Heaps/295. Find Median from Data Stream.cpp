c6075803897eeabb64d8210562eacf6c1a2eb097
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-07-19 15:09:00 +0530

    Update 295. Find Median from Data Stream.cpp

--- Heaps/295. Find Median from Data Stream.cpp
+++ Heaps/295. Find Median from Data Stream.cpp
@@ -1,3 +1,55 @@
+// optimal approach:
+// time : o(NlogN)
+class MedianFinder {
+private:
+priority_queue<int> firstHalf;
+priority_queue<int,vector<int>,greater<int>> secondHalf;
+int size;
+public:
+    MedianFinder() {
+        // constructor called:
+        //  if u wanna keep a size makes it easy to calculate;
+        size=0;
+    }
+    
+    void addNum(int num) {
+        secondHalf.push(num);
+        size++;
+
+        if(!firstHalf.empty() && secondHalf.top()<firstHalf.top()){
+            // needs rearranging between the tops of heaps
+            firstHalf.push(secondHalf.top());
+            secondHalf.pop();
+        }
+
+        if(size%2==0 && secondHalf.size()>firstHalf.size()+1){
+            // even size case
+            firstHalf.push(secondHalf.top());
+            secondHalf.pop();
+        }
+        else if(firstHalf.size()>secondHalf.size()){
+            // odd size cases
+            secondHalf.push(firstHalf.top());
+            firstHalf.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(size&1){
+            // if size is odd
+            return (double)secondHalf.top();
+        }
+
+        double med=(double)(firstHalf.top()+secondHalf.top())/2.0;
+
+        return med;
+    }
+};
+
+
+
+
+
 // APPROACH 1 :USING BRUTE FORCE AND SORTING
 
 // time : O(n*n*logn)
