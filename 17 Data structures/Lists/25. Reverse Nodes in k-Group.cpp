e152022caa98caf4a1d1cb7c32fc10059ee167af
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-04 17:26:06 +0530

    Create 25. Reverse Nodes in k-Group.cpp

---
+++ 17 Data structures/Lists/25. Reverse Nodes in k-Group.cpp
@@ -0,0 +1,35 @@
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int cnt=0;
+        ListNode*temp=head;
+
+        while(temp!=nullptr){
+            cnt++;
+            temp=temp->next;
+        }
+
+        if(cnt<k || !head){
+            return head;
+        }
+
+        // this is the node that needs to be sent to recursion
+        ListNode*cur=head;
+        ListNode*prev=nullptr;
+
+        int K=k;
+        // reverse k nodes 
+        while(K){
+            ListNode*forw=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=forw;
+            K--;
+        }
+
+        head->next=reverseKGroup(cur,k);
+
+        return prev;
+    }
+};
