acca5026bb8ed28772f5884dd236219bf773f358
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-06 15:33:28 +0530

    Update 974. Subarray Sums Divisible by K.cpp

--- Arrays/Cumulative-Prefix-Sum/974. Subarray Sums Divisible by K.cpp
+++ Arrays/Cumulative-Prefix-Sum/974. Subarray Sums Divisible by K.cpp
@@ -1,3 +1,42 @@
+// approach 2:
+// optimal approach :
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // divisible by k means modulo k==0
+        int m = k;
+
+        int n = nums.size();
+        int K=0;
+
+        // using the cumulative sum technique
+        unordered_map<int,int> cumSum;
+        cumSum[0]=1;
+
+        int runSum=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            runSum+=nums[i];
+
+            int r1=runSum%m;
+            int r2=(r1-K+m)%m;
+
+            count+=cumSum[r2];
+
+            if(r1<0) r1+=m;
+            // to avoid -1%2 in c++ is -1 not +1
+            cumSum[r1]++;
+        }
+        return count;
+    }
+};
+
+
+
+
+
+// approach 1:
 // brute force : O(n^2)
 class Solution {
 public:
