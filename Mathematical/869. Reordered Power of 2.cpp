5aba6c6a625ccf4508a49e62529db499b4ebecd1
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-10 15:48:12 +0530

    Update 869. Reordered Power of 2.cpp

--- Mathematical/869. Reordered Power of 2.cpp
+++ Mathematical/869. Reordered Power of 2.cpp
@@ -1,3 +1,38 @@
+// Generating a Unique Hash:
+class Solution {
+private:
+vector<int> primeKeys={2,3,5,7,11,13,17,19,23,29};
+int generateHash(int n){
+    long long hashVal=1;
+
+    while(n>0){
+        int dig=n%10;
+
+        hashVal*=(long long)primeKeys[dig];
+
+        n=n/10;
+    }
+
+    return hashVal;
+}
+public:
+    bool reorderedPowerOf2(int n) {
+        int target=generateHash(n);
+
+
+        for(int i=0;i<=30;i++){
+            int pow2Hash=generateHash(1<<i);
+
+            if(pow2Hash==target) return true;
+        }
+
+        return false;
+    }
+};
+
+
+
+
 // time : O(1)
 // space : O(1)
 class Solution {
