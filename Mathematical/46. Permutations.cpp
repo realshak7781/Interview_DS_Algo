0b4d08d7c0b2adc714fe447c1632aedf7025f381
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-10 14:34:26 +0530

    Update 46. Permutations.cpp

--- Mathematical/46. Permutations.cpp
+++ Mathematical/46. Permutations.cpp
@@ -1,3 +1,31 @@
+// Using recursion+backtracking:
+class Solution {
+private:
+vector<vector<int>> res;
+void solve(int idx,vector<int>& nums,int n){
+    if(idx>=n){
+        res.push_back(nums);
+        return;
+    }
+
+    for(int start=idx;start<n;start++){
+        swap(nums[start],nums[idx]);
+
+        solve(idx+1,nums,n);
+
+        swap(nums[start],nums[idx]);
+    }
+}
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        solve(0,nums,n);
+
+        return res;
+    }
+};
+
+// Using STL:
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
