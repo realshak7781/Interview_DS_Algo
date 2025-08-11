7b1b541d464cbcc261ec7c3b0225cf18e7ee196d
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-12 01:37:51 +0530

    Update 4. Median of Two Sorted Arrays.cpp

--- Arrays/Binary Search/4. Median of Two Sorted Arrays.cpp
+++ Arrays/Binary Search/4. Median of Two Sorted Arrays.cpp
@@ -1,3 +1,77 @@
+// Using only O(1) space
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        // if we use the brute force approach then we are not using the sorted
+        // nature of the arrays given to us , since linear time is not
+        // acceptable , we will think for logarithmic time and specially binary
+        // search :
+
+        // story points:
+        // we need to find out the numbers of elements from nums1 and nums2 that
+        // we can have on left side of median and right side of median
+
+        //   to equally split on left and right side of median
+        // left side count of elements=(m+n+1)/2;
+
+        int size = (m + n);
+        int idx1 = (size / 2) - 1;
+        int idx2 = (size / 2);
+        int elem1 = -1, elem2 = -1;
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                if (k == idx1) {
+                    elem1 = nums1[i];
+                } else if (k == idx2) {
+                    elem2 = nums1[i];
+                }
+                i++;
+            } else {
+                if (k == idx1) {
+                    elem1 = nums2[j];
+                } else if (k == idx2) {
+                    elem2 = nums2[j];
+                }
+                j++;
+            }
+            k++;
+        }
+
+        while (i < m) {
+            if (k == idx1) {
+                elem1 = nums1[i];
+            } else if (k == idx2) {
+                elem2 = nums1[i];
+            }
+            i++;
+            k++;
+        }
+
+        while (j < n) {
+            if (k == idx1) {
+                elem1 = nums2[j];
+            } else if (k == idx2) {
+                elem2 = nums2[j];
+            }
+            j++;
+            k++;
+        }
+
+
+        if(size&1){
+            // size is odd
+            return (double)elem2;
+        }
+
+        return (double)(elem1+elem2)/2.0;
+    }
+};
+
 
 // Brute force : O(m+n)
 // space : O(m+n)
