0488912437b968cf38ec82fb44cc7d3a2cc19dc6
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-12 01:48:41 +0530

    Update 4. Median of Two Sorted Arrays.cpp

--- Arrays/Binary Search/4. Median of Two Sorted Arrays.cpp
+++ Arrays/Binary Search/4. Median of Two Sorted Arrays.cpp
@@ -1,3 +1,69 @@
+// Most optimaL : O(log(min(m,n)))
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        // if we use the brute force approach then we are not using the sorted
+        // nature of the arrays given to us , since linear time is not
+        // acceptable , we will think for logarithmic time and specially binary
+        // search :
+
+        // story points:
+        // we need to find out the numbers of elements from nums1 and nums2 that
+        // we can have on left side of median and right side of median
+
+        //   to equally split on left and right side of median
+        // left side count of elements=(m+n+1)/2;
+
+        int totalSize=(m+n);
+
+        int low=0,high=m;
+
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            int px=mid;
+            // count of elements picked from firstv array
+            int py=(m+n+1)/2 - px;
+            // count of elements picked from second array
+
+            // for nums1 smaller array and its positioning
+            int x1= px==0?INT_MIN:nums1[px-1];
+            int x3= px==m?INT_MAX:nums1[px];
+
+            // for bigger array and its positions
+            int x2= py==0?INT_MIN:nums2[py-1];
+            int x4= py==n?INT_MAX:nums2[py];
+
+            if(x1<=x4 && x2<=x3){
+                if(totalSize&1){
+                    return (double)(max(x1,x2));
+                }
+                else{
+                    return (double)(max(x1,x2)+min(x3,x4))/2.0;
+                }
+            }
+            else if(x1>x4){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
 // Using only O(1) space
 class Solution {
 public:
