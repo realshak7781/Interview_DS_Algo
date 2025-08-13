fa22c9ffb4220fa0c2c21b653c9910c016de0021
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-13 17:59:50 +0530

    Update 326. Power of Three.cpp

--- Mathematical/Leetcode Easy/326. Power of Three.cpp
+++ Mathematical/Leetcode Easy/326. Power of Three.cpp
@@ -1,3 +1,19 @@
+// Optimal Approach :
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+
+        double logVal=log10(n)/log10(3);
+
+        return logVal==(int)logVal;
+    }
+};
+
+
+
 // Brute force approach : O(log3(N))
 class Solution {
 public:
