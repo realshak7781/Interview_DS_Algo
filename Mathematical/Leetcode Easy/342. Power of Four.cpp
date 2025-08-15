daa6265ec61683b91bb7d39bce0f8b133e8fb651
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-15 15:52:19 +0530

    Update 342. Power of Four.cpp

--- Mathematical/Leetcode Easy/342. Power of Four.cpp
+++ Mathematical/Leetcode Easy/342. Power of Four.cpp
@@ -1,3 +1,16 @@
+// optimal approach :
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+
+        double logVal=log10(n)/log10(4);
+
+        return logVal==(int)logVal;
+    }
+};
 
 // Brute force approach : O(log4(n))
 // space: O(log4(N))
