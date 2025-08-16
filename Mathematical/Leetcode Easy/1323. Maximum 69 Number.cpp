2dcdd97a9661edb1721237c501b9095577990ed5
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-16 15:20:43 +0530

    Update 1323. Maximum 69 Number.cpp

--- Mathematical/Leetcode Easy/1323. Maximum 69 Number.cpp
+++ Mathematical/Leetcode Easy/1323. Maximum 69 Number.cpp
@@ -1,3 +1,45 @@
+// using No extra space and TIME : O(2*log10(num))
+class Solution {
+private:
+int getNum(int num){
+    int n=num;
+    int sixCount=0;
+
+    while(n>0){
+        int dig=n%10;
+        if(dig==6){
+            sixCount++;
+        }
+        n/=10;
+    }
+
+
+    n=num;
+    int newNum=0;
+    int powT=1;
+    int cnt=0;
+    while(n>0){
+        int dig=n%10;
+        if(dig==6){
+            cnt++;
+            if(cnt==sixCount){
+                dig=9;
+            }
+        }
+        newNum+=(powT*dig);
+        powT*=10;
+        n/=10;
+    }
+
+    return newNum;
+}
+public:
+    int maximum69Number (int num) {
+        return getNum(num);
+    }
+};
+
+
 
 // Using String Conversion
 class Solution {
