476bc98cb0bb3a012ddc5e09753540b68173d4ba
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-08-20 19:16:50 +0530

    Update 274. H-Index.cpp

--- Binary Search/274. H-Index.cpp
+++ Binary Search/274. H-Index.cpp
@@ -1,4 +1,33 @@
 
+//Using Binary Search
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+
+        sort(begin(citations),end(citations));
+
+        int low=0,high=n-1;
+        int res=0;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int pC=n-mid;
+            int hI=citations[mid];
+
+            if(hI>=pC){
+                res=pC;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+
+        return res;
+    }
+};
 
 // Brute force : time : O(n^2)
 class Solution {
