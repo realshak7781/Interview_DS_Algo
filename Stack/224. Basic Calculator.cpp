6f5efdfd24ea5d5c5b9b37d653711b088049b8b5
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-09-05 23:47:39 +0530

    Update 224. Basic Calculator.cpp

--- Stack/224. Basic Calculator.cpp
+++ Stack/224. Basic Calculator.cpp
@@ -1,4 +1,57 @@
 
+// Approach 2: Iterative Stack based approach 
+class Solution {
+public:
+    int calculate(string s) {
+        // to avoid the recursion call stack we will explicitly use stack data structure
+
+        stack<int> st;
+        int res=0;
+        int number=0;
+        int sign=1;
+
+        for(int i=0;i<s.size();i++){
+            char cur=s[i];
+
+            if(isdigit(cur)){
+                number=number*10+(cur-'0');
+            }
+            else if(cur=='+'){
+                res+= sign*number;
+                sign=1;
+                number=0;
+            }
+            else if(cur=='-'){
+                res+=sign*number;
+                sign=-1;
+                number=0;
+            }
+            else if(cur=='('){
+                st.push(res);
+                st.push(sign);
+                res=0;
+                sign=1;
+            }
+            else if(cur==')'){
+                res+=sign*number;
+                number=0;
+
+                res=res*st.top();
+                st.pop();
+
+                res+=st.top();
+                st.pop();
+            }
+            else{
+                // spaces
+                continue;
+            }
+        }
+
+        res+=sign*number;
+        return res;
+    }
+};
 
 
 
