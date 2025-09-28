71fc5f313ea9e0a43421163c2cdcdfd263d9151d
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-09-28 22:52:54 +0530

    Update 530. Minimum Absolute Difference in BST.cpp

--- Trees/Questions/530. Minimum Absolute Difference in BST.cpp
+++ Trees/Questions/530. Minimum Absolute Difference in BST.cpp
@@ -1,6 +1,27 @@
 // IOptimal : Using DFS
 
+class Solution {
+private:
+int diff=INT_MAX;
+TreeNode*prev=nullptr;
 
+void solve(TreeNode*root){
+    if(!root) return;
+
+    solve(root->left);
+    if(prev){
+        diff=min(diff,abs(root->val-prev->val));
+    }
+    prev=root;
+    solve(root->right);
+}
+public:
+    int getMinimumDifference(TreeNode* root) {
+        solve(root);
+
+        return diff;
+    }
+};
 
 // Brute force :
 class Solution {
