d570344e143e329d76aa5cf1b7957e783ee16fe3
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-09-30 13:38:02 +0530

    Create 2221. Find Triangular Sum of an Array.cpp

---
+++ 12 Combinatorics/Questions/2221. Find Triangular Sum of an Array.cpp
@@ -0,0 +1,21 @@
+
+// Brute force approach : O(n-1)*O(n)
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n=nums.size();
+
+        while(nums.size()>1){
+            int newSize=nums.size()-1;
+            vector<int> temp(newSize);
+
+            for(int i=0;i<newSize;i++){
+                temp[i]=(nums[i]+nums[i+1])%10;
+            }
+            nums.resize(newSize);
+            nums=temp;
+        }
+
+        return nums[0];
+    }
+};
