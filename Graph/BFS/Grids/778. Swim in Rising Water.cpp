f0420063da897d176b2f7ee2cb4051fb146346ec
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-06 22:01:27 +0530

    Create 778. Swim in Rising Water.cpp

---
+++ Graph/BFS/Grids/778. Swim in Rising Water.cpp
@@ -0,0 +1,71 @@
+
+
+
+
+// Using BFS (shortest finding path algo) + Binary Search
+class Solution {
+private:
+int n;
+vector<vector<int>> dir={{0,1},{0,-1},{1,0},{-1,0}};
+bool isPossible(vector<vector<int>>& grid,int time){
+    queue<pair<int,int>> q;
+    set<pair<int,int>> vis;
+    q.push({0,0});
+    vis.insert({0,0});
+
+    while(!q.empty()){
+        int size=q.size();
+
+        for(int i=0;i<size;i++){
+            auto top=q.front();
+            q.pop();
+            int x=top.first;
+            int y=top.second;
+
+            if(x==n-1 && y==n-1) return true;
+
+            for(auto d:dir){
+                int nx=x+d[0];
+                int ny=y+d[1];
+
+                if(nx>=0 && nx<n && ny>=0 && ny<n && !vis.count({nx,ny}) && grid[nx][ny]<=time){
+                    vis.insert({nx,ny});
+                    q.push({nx,ny});
+                }
+            }
+        }
+    }
+
+    return false;
+}
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        n=grid.size();
+
+        int maxHeight=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                maxHeight=max(maxHeight,grid[i][j]);
+            }
+        }
+
+        int high=maxHeight;
+        int low=grid[0][0];
+
+        int res=-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(isPossible(grid,mid)){
+                res=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+        return res;
+    }
+};
