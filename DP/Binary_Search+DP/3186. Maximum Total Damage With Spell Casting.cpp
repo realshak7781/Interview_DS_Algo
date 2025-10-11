3fc9b2104c0af4c6279250301819605b1f82cafa
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-11 16:54:07 +0530

    Update 3186. Maximum Total Damage With Spell Casting.cpp

--- DP/Binary_Search+DP/3186. Maximum Total Damage With Spell Casting.cpp
+++ DP/Binary_Search+DP/3186. Maximum Total Damage With Spell Casting.cpp
@@ -1,5 +1,43 @@
 
 
+// Using Binary Search + DP : TIME : O(N) + O(MLogM)
+using ll=long long;
+class Solution {
+private:
+unordered_map<ll,ll> freq;
+vector<ll> dp;
+ll solve(int idx,vector<int>&v){
+    if(idx>=v.size()){
+        return 0;
+    }
+
+    if(dp[idx]!=-1) return dp[idx];
+    ll currentDamage=freq[v[idx]]*v[idx];
+    ll pick=0;
+
+    auto it=lower_bound(v.begin()+idx,v.end(),v[idx]+3);
+    int nextIdx=distance(v.begin(),it);
+
+    pick=currentDamage+solve(nextIdx,v);
+
+    ll notPick=solve(idx+1,v);
+
+    return dp[idx]=max(pick,notPick);
+}
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        int n=power.size();
+
+        for(ll val:power) freq[val]++;
+        vector<int> v;
+        for(auto it:freq) v.push_back(it.first);
+        sort(begin(v),end(v));
+
+        dp.resize(n,-1);
+        return solve(0,v);
+    }
+};
+
 
 // Brute force Approach : (exponential time complexity)
 using ll=long long;
