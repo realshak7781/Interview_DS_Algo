50596f3ff112d2911b4ae0d564c07d78e095f626
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-14 15:58:57 +0530

    Update 3349. Adjacent Increasing Subarrays Detection I.cpp

--- Arrays/Sliding Window/Easy/3349. Adjacent Increasing Subarrays Detection I.cpp
+++ Arrays/Sliding Window/Easy/3349. Adjacent Increasing Subarrays Detection I.cpp
@@ -1,6 +1,55 @@
 // APPROACH 2:
 // USING SLIDING WINDOW + HASHSET
+// T.C:O(nlogn)
+class Solution {
+private:
+bool check(vector<int>&indices,int k){
+     unordered_set<int> st;
 
+        // check for the last index
+        for(int i=0;i<indices.size();i++){
+            int curIdx=indices[i];
+            int prevIdx=curIdx-k;
+
+            if(st.count(prevIdx)) return true;
+
+            st.insert(curIdx);
+        }
+
+        return false;
+}
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> indices;
+
+
+        // when k=1
+        if(k==1){
+            for(int i=0;i<n;i++) indices.push_back(i);
+
+            return check(indices,k);
+        }
+
+        // when k>1
+        int left=0;
+        for(int right=1;right<n;right++){
+            if(nums[right]>nums[right-1]){
+                if((right-left+1)==k){
+                    indices.push_back(left);
+                    left++;
+                }
+            }
+            else if(nums[right]<=nums[right-1]){
+                left=right;
+            }
+        }
+
+       
+
+       return check(indices,k);
+    }
+};
 
 
 
