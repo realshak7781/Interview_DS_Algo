53635c66147ea2a96a8af655a050f11bd144c885
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-22 22:38:12 +0530

    Update 3346. Maximum Frequency of an Element After Performing Operations I.cpp

--- Arrays/Cumulative-Prefix-Sum/3346. Maximum Frequency of an Element After Performing Operations I.cpp
+++ Arrays/Cumulative-Prefix-Sum/3346. Maximum Frequency of an Element After Performing Operations I.cpp
@@ -1,3 +1,54 @@
+
+
+// Better Intuitive Approach : Using Difference Array technique
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+         int maxVal = *max_element(begin(nums), end(nums)) + k;
+
+        vector<int> diff(maxVal+2, 0);
+        unordered_map<int, int> freq;
+
+        for(int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+
+            int l = max(nums[i]-k, 0);
+            int r = min(nums[i]+k, maxVal);
+
+            diff[l]++;
+            diff[r+1]--;
+        }
+
+
+        int result = 1;
+
+        for(int target = 0; target <= maxVal; target++) {
+            diff[target] += (target > 0 ? diff[target-1] : 0);
+
+            int targetFreq     = freq[target];
+            int needConversion = diff[target] - targetFreq;
+
+            int maxPossibleFreq = min(needConversion, numOperations);
+
+            result = max(result, targetFreq + maxPossibleFreq);
+        }
+
+        return result;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Optimal Approach : Using the concept of Cumulative Sum :
 // Time : O(maxi)
 // space : o(maxi)
