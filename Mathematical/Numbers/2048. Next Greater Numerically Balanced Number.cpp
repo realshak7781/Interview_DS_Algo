da6ca1dba75aa768c4ad8a7ba0e405143b89e9c5
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-24 15:34:53 +0530

    Update 2048. Next Greater Numerically Balanced Number.cpp

--- Mathematical/Numbers/2048. Next Greater Numerically Balanced Number.cpp
+++ Mathematical/Numbers/2048. Next Greater Numerically Balanced Number.cpp
@@ -1,5 +1,45 @@
 
 
+//  A little optimised by finding the upper limit of numerically balanced Integer :
+class Solution {
+private:
+bool isBalanced(int num){
+    vector<int> freq(10,0);
+
+    while(num>0){
+        int dig=num%10;
+        freq[dig]++;
+        num/=10;
+    }
+
+    for(int i=0;i<10;i++){
+        if(freq[i]!=0 && freq[i]!=i) return false;
+    }
+
+    return true;
+}
+public:
+    int nextBeautifulNumber(int n) {
+
+        if(n==0) return 1;
+        if(n<21) return 22;
+        
+        int maxPossible=1224444;
+        int num=n+1;
+        while(num<=maxPossible){
+
+            if(isBalanced(num)){
+                return num;
+            }
+            else{
+                num++;
+            }
+        }
+
+        return maxPossible;
+    }
+};
+
 // Time : O(maxLimit*log(num))
 // Brute force approach :
 class Solution {
