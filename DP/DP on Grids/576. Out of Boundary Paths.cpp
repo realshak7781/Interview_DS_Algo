c04238c02171c043bc83daed4fc4d3b68f8eea34
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-24 21:46:07 +0530

    Update 576. Out of Boundary Paths.cpp

--- DP/DP on Grids/576. Out of Boundary Paths.cpp
+++ DP/DP on Grids/576. Out of Boundary Paths.cpp
@@ -1,3 +1,52 @@
+// Using Iterative Bottom Up approach :
+
+class Solution {
+private:
+    int m, n;
+    vector<vector<int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    const int mod = 1e9 + 7;
+
+int bottomUp(int startRow, int startColumn, int maxMove) {
+
+    vector<vector<vector<int>>> dp(maxMove + 1, vector<vector<int>>(m, vector<int>(n, 0)));
+
+   for(int k=1;k<=maxMove;k++){
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    int currentOut=0;
+
+                    for(auto d:dir){
+                        int nx=i+d[0];
+                        int ny=j+d[1];
+
+                        if(nx<0 || nx>=m || ny<0 || ny>=n){
+                            currentOut=(currentOut+1)%mod;
+                        }
+                        else{
+                            currentOut= (currentOut+dp[k-1][nx][ny])%mod;
+                        }
+                    }
+
+                    dp[k][i][j]=currentOut%mod;
+                }
+            }
+   }
+
+   int res=dp[maxMove][startRow][startColumn];
+
+   return  res;
+}
+
+public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        this->m = m;
+        this->n = n;
+
+        return bottomUp(startRow, startColumn, maxMove);
+    }
+};
+
+
 
 // Using Recursion and Memoization
 class Solution {
