15186ee04861e5d9d149fe4760ed0902cbaebf54
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-25 16:01:00 +0530

    Update 1716. Calculate Money in Leetcode Bank.cpp

--- Mathematical/Leetcode Easy/1716. Calculate Money in Leetcode Bank.cpp
+++ Mathematical/Leetcode Easy/1716. Calculate Money in Leetcode Bank.cpp
@@ -1,4 +1,26 @@
+// Mathematical Approach :
 
+class Solution {
+public:
+    int totalMoney(int n) {
+        // Money earnt in 7 days
+        int sevenDays= (7*(7+1))/2;
+
+        int weeks=n/7;
+        int remDays=n%7;
+
+        int totalProfit=0;
+
+        totalProfit+=(weeks*sevenDays);
+        totalProfit+= 7*((weeks*(weeks-1))/2);
+
+        // for remaining days
+        for(int d=1;d<=remDays;d++){
+            totalProfit+=(weeks+d);
+        }
+        return totalProfit;
+    }
+};
 
 // Brute force : A for loop
 class Solution {
