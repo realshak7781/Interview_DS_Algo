e4b250bbcd96ee1edf1c0ed8ed84e5694f0d6fa7
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-28 16:45:58 +0530

    Update 3354. Make Array Elements Equal to Zero.cpp

--- 04 Prefix Sums/Questions/Geometry/3354. Make Array Elements Equal to Zero.cpp
+++ 04 Prefix Sums/Questions/Geometry/3354. Make Array Elements Equal to Zero.cpp
@@ -1,4 +1,41 @@
 
+// O(N) solutoion using prefix and suffix sum
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> pre(n,0);
+        vector<int> suff(n,0);
+
+        pre[0]=nums[0];
+        suff[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            pre[i]=nums[i]+pre[i-1];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            suff[i]=suff[i+1]+nums[i];
+        }
+
+        int valid=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0) continue;
+
+            int leftSum=(i>0 ? pre[i-1]:0);
+            int rightSum=(i<n-1 ? suff[i+1]:0);
+
+            if(leftSum==rightSum){
+                valid+=2;
+            }
+            else if(abs(leftSum-rightSum)==1){
+                valid+=1;
+            }
+        }
+
+        return valid;
+    }
+};
 
 // Brute force : O(N^2) solution
 class Solution {
