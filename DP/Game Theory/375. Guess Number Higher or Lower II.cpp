9e478d7071332a253e8f109e98682ef773e15258
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-10-29 23:47:46 +0530

    Update 375. Guess Number Higher or Lower II.cpp

--- DP/Game Theory/375. Guess Number Higher or Lower II.cpp
+++ DP/Game Theory/375. Guess Number Higher or Lower II.cpp
@@ -1,4 +1,37 @@
 
+// Using Botom Up approach:
+class Solution {
+private:
+
+    int bottomUp(int n) {
+        // state definition : dp[start][end] : min amount of money to win
+        // regardless of what number one chooses in this range
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for (int len = 2; len <= n; len++) {
+
+            for (int start = 1; start <= (n - len + 1); start++) {
+                int end = start + len - 1;
+
+                int minCash = INT_MAX;
+                for (int i = start; i <= end; i++) {
+                    int leftCash = dp[start][i - 1];
+                    int rightCash = dp[i + 1][end];
+
+                    int maxi = max(leftCash, rightCash);
+                    minCash = min(minCash, i + maxi);
+                }
+
+                dp[start][end]=minCash;
+            }
+        }
+
+        return dp[1][n];
+    }
+
+public:
+    int getMoneyAmount(int n) { return bottomUp(n); }
+};
 
 // Using Recursion + Memoization
 class Solution {
