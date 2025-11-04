774f7fee5ab7b39d6e85487cbf43c2c4aa599c33
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-11-04 17:07:40 +0530

    Update 3318. Find X-Sum of All K-Long Subarrays I.cpp

--- Heaps/3318. Find X-Sum of All K-Long Subarrays I.cpp
+++ Heaps/3318. Find X-Sum of All K-Long Subarrays I.cpp
@@ -1,4 +1,84 @@
+// O((n-k)*(logk+logk + k))
+using p=pair<int,int>;
+class Solution {
+private:
+multiset<p> ms;
+unordered_map<int,int> freq;
+int calculateXSum(int x){
+    int xSum=0;
+    int count=0;
+    for(auto it=ms.rbegin();it!=ms.rend()&& count<x;it++){
+        int freq=it->first;
+        int elem=it->second;
 
+        xSum+=(freq*elem);
+        count++;
+    }
+
+    return xSum;
+}
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        int n=nums.size();
+
+        for(int i=0;i<k;i++){
+            freq[nums[i]]++;
+        }
+        for(auto it:freq){
+            ms.insert({it.second,it.first});
+        }
+
+        vector<int> ans(n-k+1,0);
+        ans[0]=calculateXSum(x);
+
+
+        for(int i=1;i<(n-k+1);i++){
+            int prevIdx=i-1;
+            int nextIdx=i+k-1;
+
+            // first erase both the prev elem and next elem
+            int prevElem=nums[prevIdx];
+            int nextElem=nums[nextIdx];
+            int prevFreq=freq[prevElem];
+
+            if (prevElem == nextElem) {
+            ans[i] = ans[i-1];
+            continue; 
+            }
+
+            auto prevP=ms.find({prevFreq,prevElem});
+            if(prevP!=ms.end()){
+                ms.erase(prevP);
+            }
+
+            
+            int nextFreq=freq[nextElem];
+            auto nextP=ms.find({nextFreq,nextElem});
+
+            if(nextP!=ms.end()){
+                ms.erase(nextP);
+            }
+
+            // now add new freq of both prev and next elem
+            freq[prevElem]--;
+
+            if(freq[prevElem]==0){
+                freq.erase(prevElem);
+            }
+            else{
+                ms.insert({freq[prevElem],prevElem});
+            }
+
+            freq[nextElem]++;
+            ms.insert({freq[nextElem],nextElem});
+
+            ans[i]=calculateXSum(x);
+        }
+
+
+        return ans;
+    }
+};
 
 
 // Brute force (O(N*(N+N*logx))
