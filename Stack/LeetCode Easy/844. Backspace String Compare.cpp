12297ae6506f50a8a34ffb0e5c0455e2b14fd955
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-11-21 16:01:44 +0530

    Update 844. Backspace String Compare.cpp

--- Stack/LeetCode Easy/844. Backspace String Compare.cpp
+++ Stack/LeetCode Easy/844. Backspace String Compare.cpp
@@ -1,3 +1,37 @@
+// Usibg Rolling Hash Approach to achieve O(1) space
+using ll=long long;
+class Solution {
+private:
+ll computeHash(const string &s){
+    ll hash=0;
+    ll w=1;
+    ll p=31;
+    const ll mod=1e9+7;
+    int skip=0;
+
+    for(int i=s.length()-1;i>=0;i--){
+        char c=s[i];
+        if(c=='#'){
+            skip++;
+        }
+        else{
+            if(skip>0){
+                skip--;
+            }
+            else{
+                hash=(hash + (c-'a'+1)*w)%mod;
+                w=(w*p)%mod;
+            }
+        }
+    }
+
+    return hash;
+}
+public:
+    bool backspaceCompare(string s, string t) {
+        return computeHash(s) == computeHash(t);
+    }
+};
 
 // Approach 1 : Using O(n) space
 class Solution {
