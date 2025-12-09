55cc5a4c8c650af6e3a29e1a578afcf560979c69
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-12-09 15:29:17 +0530

    Update 3583. Count Special Triplets.cpp

--- POTD/Hash Table/3583. Count Special Triplets.cpp
+++ POTD/Hash Table/3583. Count Special Triplets.cpp
@@ -1,4 +1,27 @@
+// A Better O(n) approach using two hashmaps:
+class Solution {
+private:
+const int mod=1e9+7;
+public:
+    int specialTriplets(vector<int>& nums) {
+        int n=nums.size();
 
+        int count=0;
+        unordered_map<int,int> rightFreq;
+        unordered_map<int,int> leftFreq;
+
+        for(int val:nums) rightFreq[val]++;
+
+        for(int mid:nums){
+            rightFreq[mid]--;
+            long long sameElem=(long long)mid*2;
+
+            count=(count+1LL*rightFreq[sameElem]*leftFreq[sameElem])%mod;
+            leftFreq[mid]++;
+        }
+        return count;
+    }
+};
 
 // Brute Force : O(n^2) : TLE
 class Solution {
