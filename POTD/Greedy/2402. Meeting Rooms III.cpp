b161d7560849e3e74fdac52682cb3f0dc9444095
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2025-12-29 16:00:13 +0530

    Update 2402. Meeting Rooms III.cpp

--- POTD/Greedy/2402. Meeting Rooms III.cpp
+++ POTD/Greedy/2402. Meeting Rooms III.cpp
@@ -1,3 +1,73 @@
+// approach 2 : using minHeap 
+using ll=long long;
+using p=pair<ll,int>;
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        vector<int> used(n,0);
+
+        sort(begin(meetings),end(meetings));
+        // sorting based on start time
+
+        priority_queue<int,vector<int>,greater<int>> available;
+        // minHeap for available rooms
+        priority_queue<p,vector<p>,greater<p>> nextAvailable;
+
+        // push initially all the available rooms
+        for(int r=0;r<n;r++){
+            available.push(r);
+        }
+
+        for(auto m:meetings){
+            int start=m[0];
+            int end=m[1];
+
+            // remember the range is not inclusive for the end part : [start,end)
+
+            // empty the rooms for this start Time
+            while(!nextAvailable.empty() && nextAvailable.top().first<=start){
+                available.push(nextAvailable.top().second);
+                nextAvailable.pop();
+            }
+
+            if(!available.empty()){
+                // rooms are available : so pick the smallest numbered available room
+                int roomIdx=available.top();
+                available.pop();
+                used[roomIdx]++;
+
+                nextAvailable.push({end,roomIdx});
+            }
+            else{
+                // no rooms were available just pick the next smallest  or earliest time when the rooms will be available
+                auto [endTime,roomIdx]=nextAvailable.top();
+                nextAvailable.pop();
+
+                used[roomIdx]++;
+                int duration=end-start;
+                ll nextAvailTime=endTime+duration;
+                nextAvailable.push({nextAvailTime,roomIdx});
+            }
+        }
+
+
+        // just find the smallest idx room most used
+        int resIdx=INT_MAX;
+        int maxVal=INT_MIN;
+        for(int r=0;r<n;r++){
+            if(used[r]>maxVal){
+                maxVal=used[r];
+                resIdx=r;
+            }
+            else if(used[r]==maxVal){
+                resIdx=min(resIdx,r);
+            }
+        }
+
+        return resIdx;
+    }
+};
+
 // using simple story points + sorting
 // time : O(n*m)
 // space : O(n)
