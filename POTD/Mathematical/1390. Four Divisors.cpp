1a55f13c4f3bce293c7ad756b2c8134a0ed3059b
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-01-05 02:15:44 +0530

    Update 1390. Four Divisors.cpp

--- POTD/Mathematical/1390. Four Divisors.cpp
+++ POTD/Mathematical/1390. Four Divisors.cpp
@@ -1,3 +1,43 @@
+// Approach 2 : Using square root mathematics : O(n*sqrt(val))
+class Solution {
+private:
+    int getSum(int val) {
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i * i <= val; i++) {
+            if (val % i == 0) {
+                int div1 = i;
+                int div2 = val / i;
+
+                if (div1 == div2) {
+                    sum += div1;
+                    count += 1;
+                } else {
+                    sum += div1 + div2;
+                    count += 2;
+                }
+            }
+
+            if (count > 4)
+                return 0;
+        }
+
+        return count == 4 ? sum : 0;
+    }
+
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int totalSum = 0;
+
+        for (int val : nums) {
+            totalSum += getSum(val);
+        }
+
+        return totalSum;
+    }
+};
+
 
 // approach 1 : O(N*max(nums))
 class Solution {
