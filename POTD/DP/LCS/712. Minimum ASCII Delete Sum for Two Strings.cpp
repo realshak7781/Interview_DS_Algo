777cd6399e9e317199302d03fa90e25292b9edda
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-01-11 02:52:18 +0530

    Update 712. Minimum ASCII Delete Sum for Two Strings.cpp

--- POTD/DP/LCS/712. Minimum ASCII Delete Sum for Two Strings.cpp
+++ POTD/DP/LCS/712. Minimum ASCII Delete Sum for Two Strings.cpp
@@ -1,3 +1,64 @@
+// Using Tabulation approach:
+// time : O(n*m)
+// apace :O(n*m)
+class Solution {
+private:
+
+    int bot(string &s1, string &s2) {
+        if (s1.length() < s2.length()) {
+            swap(s1, s2);
+            // always keep s1 longer than s2
+        }
+
+        int m = s1.length(), n = s2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // state definition :dp[i][j]: be the answer for string s1 [i:] and
+        // string s2[j:]
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int curAsciiSum = 0;
+                // two values i,j are equal
+                // we will take that value in our sub sequence and try to
+                // maximize this ascii value sum
+                if (s1[i] == s2[j]) {
+                    curAsciiSum =
+                        max(curAsciiSum, int(s1[i]) + dp[i + 1][j + 1]);
+                }
+                // they are not equal
+                else {
+                    curAsciiSum = max(curAsciiSum, dp[i + 1][j]);
+                    curAsciiSum = max(curAsciiSum, dp[i][j + 1]);
+                }
+
+                dp[i][j] = curAsciiSum;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+public:
+    int minimumDeleteSum(string s1, string s2) {
+
+        int lenToKeep = bot(s1,s2);
+
+        int len1 = 0;
+        for (char c : s1) {
+            len1 += int(c);
+        }
+
+        int len2 = 0;
+        for (char c : s2) {
+            len2 += int(c);
+        }
+
+        int len2Del = len1 + len2 - 2 * lenToKeep;
+
+        return len2Del;
+    }
+};
+
 
 // Using Recursion + Memoization
 // time : O(n*m)
