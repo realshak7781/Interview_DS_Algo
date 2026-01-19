06973e45331f507af574cfc0f57faeb3b41accd8
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-01-19 23:25:59 +0530

    Create 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.cpp

---
+++ POTD/Mathematical/Geometry/1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold.cpp
@@ -0,0 +1,53 @@
+
+
+
+// Using Brute Force + Prefix sum 
+// TIME : o(LEN*M*N)
+// SPACE : O(M*N)
+class Solution {
+private:
+vector<vector<int>> pref;
+
+int getSum(int r1,int c1,int r2,int c2){
+    return (pref[r2+1][c2+1] - pref[r2+1][c1]-pref[r1][c2+1] + pref[r1][c1]);
+}
+
+bool check(int r1,int r2,int c1,int c2,vector<vector<int>>& mat, int threshold){
+    int sqSubMatSum=getSum(r1,c1,r2,c2);
+
+
+    return sqSubMatSum<=threshold;
+}
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int m=mat.size();
+        int n=mat[0].size();
+
+        pref.resize(m+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                pref[i][j]=mat[i-1][j-1]+pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
+            }
+        }
+
+        int maxSideLen=min(m,n);
+        
+        for(int len=maxSideLen;len>=1;len--){
+           for(int i=0;i<=(m-len);i++){
+                // sliding window on rows
+                for(int j=0;j<=(n-len);j++){
+                    // sliding windows on cols
+                    int rS=i;
+                    int rE=i+len-1;
+                    int cS=j;
+                    int cE=j+len-1;
+
+                    if(check(rS,rE,cS,cE,mat,threshold)) return len;
+                }
+            }
+        }
+
+        return 0;
+    }
+};
