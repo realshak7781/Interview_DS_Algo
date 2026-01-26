6d6b2666c52353ff098e7ce1afe04640a9070693
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-01-26 21:51:07 +0530

    Update 1200. Minimum Absolute Difference.cpp

--- POTD/Greedy/Sorting/1200. Minimum Absolute Difference.cpp
+++ POTD/Greedy/Sorting/1200. Minimum Absolute Difference.cpp
@@ -1,4 +1,47 @@
 
+// USING HASHMAP TO REDUCE THE BRUTE FORCE
+// TIME : O(NLOGN)
+// SPACE : O(N)
+class Solution {
+private:
+int getMinAbsDiff(vector<int>& arr,int n){
+    int minDiff=INT_MAX;
+
+    for(int i=0;i<n-1;i++){
+        int low=i;
+        int high=i+1;
+
+        minDiff=min(minDiff,arr[high]-arr[low]);
+    }
+    return minDiff;
+}
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+
+        int minAbsDiff=getMinAbsDiff(arr,n);
+
+        vector<vector<int>> res;
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++){
+            // b-a=diff
+            int b=arr[i];
+            int a=b-minAbsDiff;
+
+            if(mp.count(a)){
+                res.push_back({a,b});
+            }
+
+            mp[b]=i;
+        }
+        return res;
+    }
+};
+
+
+
 // BRUTE FORCE APPROACH : TIME : O(N*N)+O(NLOGN)
 // SPACE : O(1)
 class Solution {
