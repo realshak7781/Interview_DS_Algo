9c6bc8d448c12e2521192d6a4f0d1d0a41c3bb40
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-02-04 17:54:30 +0530

    Update 3013. Divide an Array Into Subarrays With Minimum Cost II.cpp

--- POTD/Greedy/Sorting/3013. Divide an Array Into Subarrays With Minimum Cost II.cpp
+++ POTD/Greedy/Sorting/3013. Divide an Array Into Subarrays With Minimum Cost II.cpp
@@ -1,3 +1,91 @@
+// Using Sliding window and Two setss
+// TIME :O(N*LOG(DIST))
+// SPACE : O(DIST)
+using ll=long long;
+class Solution {
+private:
+public:
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        ll totalMinCost=LLONG_MAX;
+        int n=nums.size();
+
+        multiset<ll> L; /*stores k-1 elements */
+        multiset<ll> R; /*stores rem elements */
+
+        for(int i=1;i<=(dist+1);i++){
+            if(L.size()<(k-1)){
+                L.insert(1LL*nums[i]);
+            }
+            else{
+                ll maxElem=*L.rbegin();
+                if(nums[i]<maxElem){
+                    R.insert(maxElem);
+                    L.erase(L.find(maxElem));
+                    L.insert(nums[i]);
+                }
+                else{
+                    R.insert(nums[i]);
+                }
+            }
+        }
+
+        ll curCost=nums[0];
+        for(ll val:L){
+            curCost+=val;
+        }
+
+        totalMinCost=curCost;
+
+        // sliding window logic here
+        for(int i=dist+2;i<n;i++){
+            // using sliding window + efficient k-2 minimum cost retrieva
+            ll prevElem=nums[i-dist-1];
+            ll curElem=nums[i];
+
+            // remove the outgoing element
+            if(L.find(prevElem)!=L.end()){
+                auto it=L.find(prevElem);
+                L.erase(it);
+                curCost-=prevElem;
+            }
+            else if(R.find(prevElem)!=R.end()){
+                R.erase(R.find(prevElem));
+            }
+            // add the incomimg element
+            ll maxElem=*L.rbegin();
+
+            if(curElem<maxElem){
+                L.insert(curElem);
+                curCost+=curElem;
+            }
+            else{
+                R.insert(curElem);
+            }
+
+
+            // try to rebalance
+            if(L.size()<(k-1)){
+                ll minCost=*R.begin();
+                L.insert(minCost);
+                curCost+=minCost;
+                R.erase(R.find(minCost));
+            }
+            else if(L.size()>(k-1)){
+                ll maxCost=*L.rbegin();
+                curCost-=maxCost;
+                R.insert(maxCost);
+                L.erase(L.find(maxCost));
+            }
+            // update global min cost;
+
+            totalMinCost=min(totalMinCost,curCost);
+        }
+
+        return totalMinCost;
+    }
+};
+
+
 
 // SOLVING THE PART TWO OF THE PROBLEM
 // USING BRUTE FORCE APPROACH : O(N*N*LOGN)
