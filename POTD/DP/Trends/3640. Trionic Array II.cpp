fceba10760337415f464f1587f462e8761f8aa03
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-02-07 18:21:46 +0530

    Update 3640. Trionic Array II.cpp

--- POTD/DP/Trends/3640. Trionic Array II.cpp
+++ POTD/DP/Trends/3640. Trionic Array II.cpp
@@ -1,3 +1,73 @@
+// using bottom up dp:
+using ll = long long;
+class Solution {
+private:
+    const ll negInf = -1e16;
+    ll solve(vector<int>& nums) {
+        int  n=nums.size();
+        vector<vector<ll>> dp(n + 1, vector<ll>(5, negInf));
+
+        dp[n][3] = 0;
+
+        // state definition : dp[idx][trend] : maximum sum of any trionic subarray starting at idx eith trend t 
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int trend=3;trend>=0;trend--) {
+                ll maxSum = negInf;
+                if (trend == 0) {
+                    maxSum = max(maxSum, dp[idx+1][0]);
+                }
+
+                if (trend == 3) {
+                    maxSum = nums[idx];
+                }
+                if (idx + 1 < n) {
+                    int cur = nums[idx];
+                    int next = nums[idx + 1];
+
+                    if (trend == 0 && next > cur) {
+                        maxSum =
+                            max(maxSum, nums[idx] + dp[idx+1][1]);
+                    } else if (trend == 1) {
+                        if (next > cur) {
+                            maxSum = max(
+                                maxSum, nums[idx] + dp[idx+1][1]);
+                        } else if (next < cur) {
+                            maxSum = max(
+                                maxSum, nums[idx] + dp[idx+1][2]);
+                        }
+                    } else if (trend == 2) {
+                        if (next < cur) {
+                            maxSum = max(
+                                maxSum, nums[idx] + dp[idx+1][2]);
+                        } else if (next > cur) {
+                            maxSum = max(
+                                maxSum, nums[idx] + dp[idx+1][3]);
+                        }
+                    } else if (trend == 3) {
+                        if (next > cur) {
+                            // continuing trend
+                            maxSum = max(
+                                maxSum, nums[idx] + dp[idx+1][3]);
+                        }
+                    }
+                }
+
+               dp[idx][trend] = maxSum;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+public:
+    long long maxSumTrionic(vector<int>& nums) {
+        int n = nums.size();
+        return solve(nums);
+    }
+};
+
+
+// Using Recursion + Memoization
 using ll=long long;
 class Solution {
 private :
