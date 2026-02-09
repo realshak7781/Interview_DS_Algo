afc5cf1f3ecc64b4373c6a26f83298b25b4794ab
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-02-09 15:54:41 +0530

    Update 1653. Minimum Deletions to Make String Balanced.cpp

--- POTD/Stack/1653. Minimum Deletions to Make String Balanced.cpp
+++ POTD/Stack/1653. Minimum Deletions to Make String Balanced.cpp
@@ -1,4 +1,28 @@
+// Using stack
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        // trying to keep all the a's to the left and all the b's to the right
+        // at  any point we will count how many a's we have on the right side
+        // we will also count how many b's we have on left side
+        // the deletions will be the sum of these two
 
+        int minDels=0;
+        stack<char> st;
+
+        for(int i=0;i<s.length();i++){
+            char cur=s[i];
+            if(!st.empty() && st.top()=='b' && cur=='a'){
+                st.pop();
+                minDels++;
+            }
+            else{
+                st.push(cur);
+            }
+        }
+        return minDels;
+    }
+};
 
 
 // Approach 2 : Using No extra space
