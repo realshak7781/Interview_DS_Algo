70aea289e6860b88bee31dbf769601f1ec4b8803
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-02-15 19:24:13 +0530

    Update 1943. Describe the Painting.cpp

--- Advanced Algorithms/Line Sweep/1943. Describe the Painting.cpp
+++ Advanced Algorithms/Line Sweep/1943. Describe the Painting.cpp
@@ -1,4 +1,44 @@
+// APPPROACH 2 : USING A MUTLISET AND LINE SWEEP TECHNIQUE
 
+using ll=long long;
+class Solution {
+public:
+    vector<vector<long long>> splitPainting(vector<vector<int>>& segments) {
+        multiset<pair<ll,ll>> ms;
+        sort(begin(segments),end(segments));
+
+        for(auto &seg:segments){
+            ms.insert({seg[0],seg[2]});
+            ms.insert({seg[1],-seg[2]});
+        }
+        vector<vector<ll>> res;
+        auto it=ms.begin();
+        auto firstPair=*it;
+
+        ++it;
+        ll sum=firstPair.second;
+        ll prevStart=firstPair.first;
+
+        while(it!=ms.end()){
+
+            while(it != ms.end() && it->first == prevStart){
+                sum+=it->second;
+                ++it;
+            }
+
+            if(it==ms.end()) break;
+
+            auto curPair=*it;
+            if(sum>0){
+                res.push_back({prevStart,curPair.first,sum});
+            }
+            prevStart=curPair.first;
+            sum+=curPair.second;
+            ++it;
+        }
+        return res;
+    }
+};
 
 // USING APPROACH 1 : BRUTE FORCE 
 // TIME : O(N*N)
