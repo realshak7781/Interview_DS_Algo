ade1c948685b8d57661c9f1b847f7afbf3f183d6
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-02-22 22:11:47 +0530

    Update 1109. Corporate Flight Bookings.cpp

--- Advanced Algorithms/Line Sweep/1109. Corporate Flight Bookings.cpp
+++ Advanced Algorithms/Line Sweep/1109. Corporate Flight Bookings.cpp
@@ -1,4 +1,27 @@
 
+// Optimal APProach : Using Difference Array technique
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> res(n,0);
+
+        for(auto &b:bookings){
+            int l=b[0]-1;
+            int r=b[1];
+            int seats=b[2];
+
+            res[l]+=seats;
+            if(r<n) res[r]-=seats;
+        }
+
+        for(int i=1;i<n;i++){
+            res[i]+=res[i-1];
+        }
+
+        return res;
+    }
+};
+
 // Brute force :
 // time : O(B*(O(r-l+1))
 class Solution {
