fe0e89d8a6ae58546ef3cb21fb4a3cca24e90c49
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-03-15 19:19:54 +0530

    Update 1980. Find Unique Binary String.cpp

--- POTD/Backtracking/1980. Find Unique Binary String.cpp
+++ POTD/Backtracking/1980. Find Unique Binary String.cpp
@@ -1,4 +1,73 @@
 
+// Using optimal approach by converting string s to decimals
+// Time : o(len + maxVal)
+// space : o(n)
+class Solution {
+private:
+int findPower(int a,int b){
+    if(b==0){
+        return 1;
+    }
+    int half=findPower(a,b/2);
+    int res=half*half;
+
+    if(b&1){
+        res=a*res;
+    }
+    return res;
+}
+
+
+int getValFromString(string &s){
+    int len=s.length();
+
+    int val=0;
+    int mul=1;
+    for(int i=len-1;i>=0;i--){
+        val=val + (s[i]-'0')*mul;
+        mul*=2;
+    }
+    return val;
+}
+
+string getStringFromVal(int val,int len){
+
+    string res="";
+    while(val>0){
+        int dig=val%2;
+        res+=(dig+'0');
+        val/=2;
+    }
+
+    // append zeroes
+    int n=res.length();
+    for(int i=1;i<=(len-n);i++){
+        res+='0';
+    }
+
+    reverse(res.begin(),res.end());
+
+    return res;
+}
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int len=nums.size();
+        unordered_set<int> availVal;
+        int maxVal=findPower(2,len)-1;
+
+        for(string &s:nums){
+            availVal.insert(getValFromString(s));
+        }
+
+        for(int res=0;res<=maxVal;res++){
+            if(!availVal.count(res)){
+                return getStringFromVal(res,len);
+            }
+        }
+
+        return "";
+    }
+};
 
 // USING BRUTE FORCE APPROACH :
 // BACKTRACKING
