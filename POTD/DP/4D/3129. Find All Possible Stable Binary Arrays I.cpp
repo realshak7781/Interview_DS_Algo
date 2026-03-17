95f44511d501b7b742e1ac1ee0788078a5351671
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-03-17 16:51:01 +0530

    Update 3129. Find All Possible Stable Binary Arrays I.cpp

--- POTD/DP/4D/3129. Find All Possible Stable Binary Arrays I.cpp
+++ POTD/DP/4D/3129. Find All Possible Stable Binary Arrays I.cpp
@@ -1,4 +1,40 @@
+// Using a Better little optimised Recursion + memoization approach
+const int mod=1e9+7;
+class Solution {
+private:
+vector<vector<vector<int>>> dp;
+int solve(int zero,int one,int zeroOrOne,int limit){
+    if(zero==0 && one==0){
+        return 1;
+    }
 
+    if(dp[zero][one][zeroOrOne]!=-1) return dp[zero][one][zeroOrOne];
+    long long resCnt=0;
+    if(zeroOrOne==0){
+        // use 0 atmost min(limit,zero)
+        for(int zCnt=1;zCnt<=min(limit,zero);zCnt++){
+            resCnt=(resCnt+solve(zero-zCnt,one,1,limit))%mod;
+        }
+    }
+    else{
+        // use 1 atmost min(limit,one)
+        for(int oneCnt=1;oneCnt<=min(limit,one);oneCnt++){
+            resCnt=(resCnt+solve(zero,one-oneCnt,0,limit))%mod;
+        }
+    }
+
+    return dp[zero][one][zeroOrOne]=resCnt%mod;
+}
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        dp.resize(zero+1,vector<vector<int>>(one+1,vector<int>(2,-1)));
+        int zeroStartRes=solve(zero,one,0,limit)%mod;
+        dp.resize(zero+1,vector<vector<int>>(one+1,vector<int>(2,-1)));
+        int oneStartRes=solve(zero,one,1,limit)%mod;
+
+        return (zeroStartRes+oneStartRes)%mod;
+    }
+};
 
 // using brute force approach : Memo + Recur
 const int mod = 1e9 + 7;
