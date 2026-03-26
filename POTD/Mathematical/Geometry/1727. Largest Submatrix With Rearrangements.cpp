2cbbcc3771a17f2b28b5a5e4afcae867d146a601
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-03-26 16:36:26 +0530

    Update 1727. Largest Submatrix With Rearrangements.cpp

--- POTD/Mathematical/Geometry/1727. Largest Submatrix With Rearrangements.cpp
+++ POTD/Mathematical/Geometry/1727. Largest Submatrix With Rearrangements.cpp
@@ -1,3 +1,50 @@
+// Reduce the time complexity from O(rows*cols) --------> O(cols)
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+
+
+        vector<int> prevHeight(cols,0);
+        int resArea=0;
+        for(int i=0;i<rows;i++){
+            vector<int> curHeight(cols,0);
+
+
+            // poplulatimg the curHeight
+            for(int k=0;k<cols;k++){
+                if(matrix[i][k]==0) continue;
+
+                curHeight[k]=matrix[i][k]+prevHeight[k];
+            }
+
+            prevHeight=curHeight;
+            sort(begin(curHeight),end(curHeight),greater<int>());
+            // sortting logic
+
+            int minHeight=1e9;
+            int base=0;
+
+            for(int j=0;j<cols;j++){
+                int curHeightVal=curHeight[j];
+                if(curHeightVal==0){
+                    base=0;
+                    minHeight=1e9;
+                }else{
+                    minHeight=min(curHeightVal,minHeight);
+                    base+=1;
+
+                    resArea=max(resArea,base*minHeight);
+                }
+            }
+        }
+
+
+        return resArea;
+    }
+};
+
 
 // Time : O(r*col*log(r))
 // space : O(r*c)
