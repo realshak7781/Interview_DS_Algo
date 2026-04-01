56e11f1400e532628eb196f6d8278e352ef7a829
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-04-01 22:53:21 +0530

    Update 3212. Count Submatrices With Equal Frequency of X and Y.cpp

--- POTD/Matrix/3212. Count Submatrices With Equal Frequency of X and Y.cpp
+++ POTD/Matrix/3212. Count Submatrices With Equal Frequency of X and Y.cpp
@@ -1,3 +1,65 @@
+// Optimal approach:
+// TIME : O(R*C)
+// SPACE : O(R*C)
+
+class Solution {
+private:
+vector<vector<int>> xCnt,yCnt;
+bool check(int r1,int c1,int r2,int c2){
+
+    int countX=xCnt[r2+1][c2+1]-xCnt[r1][c2+1]-xCnt[r2+1][c1]+xCnt[r1][c1];
+    int countY=yCnt[r2+1][c2+1]-yCnt[r1][c2+1]-yCnt[r2+1][c1]+yCnt[r1][c1];
+
+    return countX>0 && countX==countY;
+}
+public:
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int rows=grid.size();
+        int cols=grid[0].size();
+
+        xCnt.resize(rows+1,vector<int>(cols+1,0));
+        yCnt.resize(rows+1,vector<int>(cols+1,0));
+
+        // create the count arrays
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=cols;j++){
+                int prevX=xCnt[i-1][j]+xCnt[i][j-1]-xCnt[i-1][j-1];
+                int prevY=yCnt[i-1][j]+yCnt[i][j-1]-yCnt[i-1][j-1];
+
+                char cur=grid[i-1][j-1];
+
+                if(cur=='X'){
+                    xCnt[i][j]=1+prevX;
+                    yCnt[i][j]=prevY;
+                }
+                else if(cur=='Y'){
+                    yCnt[i][j]=1+prevY;
+                    xCnt[i][j]=prevX;
+                }
+                else{
+                    xCnt[i][j]=prevX;
+                    yCnt[i][j]=prevY;
+                }
+            }
+        }
+
+
+        int resCnt=0;
+        for(int len=0;len<cols;len++){
+            int r1=0,c1=0;
+            for(int r=0;r<rows;r++){
+                int r2=r,c2=len;
+
+                if(check(r1,c1,r2,c2)){
+                    resCnt++;
+                }
+            }
+        }
+
+        return resCnt;
+    }
+};
+
 
 // Brute force approach 
 // TIME : O(R^2 * C^2)
