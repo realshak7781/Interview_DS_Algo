fa11aa1d640ebb59025883c91d107268d95e050c
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-04-14 22:15:09 +0530

    Update 2751. Robot Collisions.cpp

--- POTD/Stack/Hard/2751. Robot Collisions.cpp
+++ POTD/Stack/Hard/2751. Robot Collisions.cpp
@@ -1,4 +1,84 @@
+// TIME : O(N)
+// SPACE : O(N)
 
+struct Node {
+    int position;
+    char direction;
+    int original_idx;
+};
+
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions,
+                                      vector<int>& healths, string directions) {
+        vector<Node> vec;
+
+        for (int i = 0; i < positions.size(); i++) {
+            vec.emplace_back(positions[i], directions[i], i);
+        }
+
+        // sort the array based on the position
+        sort(vec.begin(), vec.end(), [](const Node& a, const Node& b) {
+            return a.position < b.position;
+        });
+
+        // now collision only occurs if twp bodies are mpving in (R,L) fashion
+        int totalRobots = vec.size();
+        stack<Node> st;
+        for (int i = 0; i < totalRobots; i++) {
+            if (vec[i].direction == 'R') {
+                st.push(vec[i]);
+            } else {
+                bool destroyed = false;
+                while (!st.empty() && st.top().direction == 'R') {
+                    int cur_health = healths[vec[i].original_idx];
+                    int prev_health = healths[st.top().original_idx];
+
+                    if (prev_health > cur_health) {
+                        if (healths[st.top().original_idx] > 0) {
+                            healths[st.top().original_idx]--;
+                        }
+                        destroyed=true;
+                        break;
+                    }
+                    // current j can be small R
+                    else if (prev_health < cur_health) {
+                        if (healths[vec[i].original_idx] > 0) {
+                            healths[vec[i].original_idx]--;
+                        }
+                        st.pop();
+                    } // current j can be equal R
+                    else {
+                        st.pop();
+                        destroyed=true;
+                        break;
+                    }
+                }
+
+                if(!destroyed){
+                    st.push(vec[i]);
+                }
+            }
+        }
+
+        vector<Node> finalNodes;
+
+        while(!st.empty()){
+            finalNodes.push_back(st.top());
+            st.pop();
+        }
+
+        sort(finalNodes.begin(),finalNodes.end(),[](const Node &a,const Node &b){
+            return a.original_idx<b.original_idx;
+        });
+
+        vector<int> res;
+        for(int i=0;i<finalNodes.size();i++){
+            res.push_back(healths[finalNodes[i].original_idx]);
+        }
+        return res;
+    }
+};
 
 // TIME : O(NLOGN) + O(N*N)
 // SPACE : O(N)
