ecd67b9744ddaf2d619ba238d5418d229f50d4a0
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-04-19 17:24:42 +0530

    Create 2075. Decode the Slanted Ciphertext.cpp

---
+++ POTD/Matrix/Strings/2075. Decode the Slanted Ciphertext.cpp
@@ -0,0 +1,55 @@
+
+// TIME : O(ROWS*COLS)
+// SPACE : O(ROWS*COLS)
+// BRUTE FORCE APPROACH : USING EXTRA SPACE TO CREATE THE CIPHER MATRIX'
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int len=encodedText.length();
+        int cols=len/rows;
+
+        vector<vector<char>> cipher(rows,vector<char>(cols,' '));
+        int r=-1,c=-1;
+
+        for(int i=0;i<len;i++){
+            if(i%cols==0){
+                r++;
+            }
+            c=(c+1)%cols;
+            char cur=encodedText[i];
+
+            cipher[r][c]=cur;
+        }
+
+        // now cipher array is created
+        // get the originalText back
+
+
+        string originalTxt="";
+        int j=0;
+        while(j<cols){
+            int k=j; 
+            for(int i=0;i<rows;i++){
+                if(k>=cols) break;
+                originalTxt+=cipher[i][k];
+                k++;
+            }
+            j++;
+        }
+
+
+        // remove the trailing space 
+        int idx=-1;
+        for(int i=originalTxt.length()-1;i>=0;i--){
+            if(originalTxt[i]!=' '){
+                idx=i;
+                break;
+            }
+        }
+
+        if(idx!=-1){
+            originalTxt=originalTxt.substr(0,idx+1);
+        }
+        return originalTxt;
+    }
+};
