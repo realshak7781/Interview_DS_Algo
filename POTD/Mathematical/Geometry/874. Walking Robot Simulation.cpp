47a6e2893beb013fa823c5beafd8627bd4509366
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-04-20 23:07:02 +0530

    Create 874. Walking Robot Simulation.cpp

---
+++ POTD/Mathematical/Geometry/874. Walking Robot Simulation.cpp
@@ -0,0 +1,78 @@
+
+
+// A LITTLE BETTER APPROACH THAN THE brute force approach
+// TIME : O(C*LOG(OBS))
+// SPACE : O(OBS)
+class Solution {
+private:
+    int euclidDistance(int x, int y) {
+        int dist = x * x + y * y;
+        return dist;
+    }
+
+    string changeDirection(string& curDir, int val) {
+
+        if (curDir == "+y") {
+            return val==-1 ? "+x" : "-x";
+        } else if (curDir == "-y") {
+            return val==-1 ? "-x" : "+x";
+        } else if (curDir == "+x") {
+            return val==-1 ? "-y" : "+y";
+        } else {
+            return val==-1 ? "+y" : "-y";
+        }
+
+        return "#";
+    }
+
+    auto getIncrement(string &curDir){
+        pair<int,int> inc;
+
+        if (curDir == "+y") {
+            inc={0,1};
+        } else if (curDir == "-y") {
+            inc={0,-1};
+        } else if (curDir == "+x") {
+            inc={1,0};
+        } else {
+            inc={-1,0};
+        }
+
+        return inc;
+    }
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        set<pair<int, int>> obs;
+        for (auto& it : obstacles) {
+            obs.insert({it[0], it[1]});
+        }
+
+        int maxDist = 0;
+        string dir = "+y";
+        int i=0,j=0;
+        for (int c : commands) {
+            if(c<0){
+                dir=changeDirection(dir,c);
+            }
+            else{
+                int steps=1;
+                auto increment=getIncrement(dir);
+                while(steps<=c){
+
+                    int nxtI=i+increment.first;
+                    int nxtJ=j+increment.second;
+
+                    if(obs.count({nxtI,nxtJ})){
+                        break;
+                    }
+                    i=nxtI;
+                    j=nxtJ;
+                    steps++;
+                    maxDist=max(maxDist,euclidDistance(nxtI,nxtJ));
+                }
+            }
+        }
+
+        return maxDist;
+    }
+};
