65f60847f53f47afbe9ef4f7341fd2adefbfa961
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-04-23 18:33:01 +0530

    Update 2069. Walking Robot Simulation II.cpp

--- POTD/Mathematical/Geometry/2069. Walking Robot Simulation II.cpp
+++ POTD/Mathematical/Geometry/2069. Walking Robot Simulation II.cpp
@@ -1,4 +1,76 @@
+// Optimal Approach:
+class Robot {
+private:
+int totalVal;
+int curVal;
+int W,H;
+bool moved;
+auto getPair(int val,int W,int H){
+    pair<pair<int,int>,string> res;
 
+    if(val<W){
+        // East
+        res={{val,0},"East"};
+    }
+    else if(val>=W && val<=(W+H-2)){
+        int c=val-(W-1);
+        int r=W-1;
+
+        res={{r,c},"North"};
+    }
+    else if(val>=(W+H-1) && val<(2*W+H-2)){
+        int c=H-1;
+        int r=W-1-(val-(W+H-2));
+
+        res={{r,c},"West"};
+    }
+    else if(val>=(2*W+H-2)){
+        int c=H-1 -(val-(2*W+H-3));
+        int r=0;
+
+        res={{r,c},"South"};
+    }
+
+    return res;
+}
+public:
+    Robot(int width, int height) {
+        curVal=0;
+        totalVal=2*(width+height)-4;
+        W=width;
+        H=height;
+        moved=false;
+    }
+    
+    void step(int num) {
+        moved=true;
+        curVal=(curVal+num)%totalVal;
+    }
+    
+    vector<int> getPos() {
+        auto key=getPair(curVal,W,H);
+
+        int x=key.first.first;
+        int y=key.first.second;
+
+        return {x,y};
+    }
+    
+    string getDir() {
+        if(curVal==0 && moved) return "South";
+        auto key=getPair(curVal,W,H);
+
+        return key.second;
+    }
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
 
 // BRUTE FORCE APPROACH : O(MAX(ROWS,COLS)*STEPS)
 class Robot {
