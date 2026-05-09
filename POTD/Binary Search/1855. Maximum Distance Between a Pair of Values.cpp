12ad9e1f4aafc610aa5a96dca6084e9d1c49c330
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-05-09 22:24:30 +0530

    Update 1855. Maximum Distance Between a Pair of Values.cpp

--- POTD/Binary Search/1855. Maximum Distance Between a Pair of Values.cpp
+++ POTD/Binary Search/1855. Maximum Distance Between a Pair of Values.cpp
@@ -1,5 +1,36 @@
 // Python colution
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        m=len(nums1)
+        n=len(nums2)
 
+        maxDist=0
+
+        for i,val in enumerate(nums1):
+            j=self.getIdx(i,n,nums2,val)
+
+            if j!=-1:
+                maxDist=max(maxDist,j-i)
+        
+        return maxDist
+    
+    def getIdx(self,low:int,n:int,arr: List[int],target:int)->int:
+        if low>=n:
+            return -1
+        high=n-1
+
+        idx=-1
+
+        while low<=high:
+            mid=low+(high-low)//2
+
+            if arr[mid]>=target:
+                idx=mid
+                low=mid+1
+            else:
+                high=mid-1
+        
+        return idx
 
 // CPP solution
 class Solution {
