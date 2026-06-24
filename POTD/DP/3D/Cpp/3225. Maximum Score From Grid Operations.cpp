e51564d8c13f7844343cb91c8f59d1b15b4157b7
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-06-24 17:43:09 +0530

    Update 3225. Maximum Score From Grid Operations.cpp

--- POTD/DP/3D/Cpp/3225. Maximum Score From Grid Operations.cpp
+++ POTD/DP/3D/Cpp/3225. Maximum Score From Grid Operations.cpp
@@ -1,3 +1,58 @@
+// OPTIMAL APPROACH :
+// TIME : O(N*N*N)
+// SPACE : O(N*N*N)
+
+using ll=long long;
+class Solution {
+private:
+vector<vector<ll>> colPrefSum;
+
+ll memo[102][102][2];
+ll solve(int prev_h,int col,bool prev_taken,vector<vector<int>>& grid,int n){
+    if(col>=n){
+        return 0;
+    }
+
+    if(memo[prev_h+1][col][prev_taken]!=-1) return memo[prev_h+1][col][prev_taken];
+    ll result=0;
+    for(int height=-1;height<n;height++){
+        int cur_h=height;
+
+        ll prevScore=0,curScore=0;
+
+        if(prev_h>cur_h){
+            curScore=colPrefSum[prev_h+1][col+1]-colPrefSum[cur_h+1][col+1];
+        }
+
+        if(!prev_taken && cur_h>prev_h){
+            prevScore=colPrefSum[cur_h+1][col]-colPrefSum[prev_h+1][col];
+        }
+
+        ll curTaken=curScore+prevScore+solve(cur_h,col+1,true,grid,n);
+        ll curNotTaken=prevScore+solve(cur_h,col+1,false,grid,n);
+
+        result=max({result,curTaken,curNotTaken});
+    }
+
+    return memo[prev_h+1][col][prev_taken]=result;
+}
+public:
+    long long maximumScore(vector<vector<int>>& grid) {
+        int n=grid.size();
+        colPrefSum.resize(n+1,vector<ll>(n+1,0));
+
+        memset(memo, -1, sizeof(memo));
+        for(int col=1;col<=n;col++){
+            for(int row=1;row<=n;row++){
+                colPrefSum[row][col]=colPrefSum[row-1][col]+grid[row-1][col-1];
+            }
+        }
+        return solve(-1,0,false,grid,n);
+    }
+};
+
+
+
 // TIME : O(N^N  * N)
 // SPACE : O(N*N + N + N)
 
