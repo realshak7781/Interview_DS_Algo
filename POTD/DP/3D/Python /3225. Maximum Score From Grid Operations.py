74e41a26cba812677fc4ffeb795cc3c389d5dbc9
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-06-24 18:37:48 +0530

    Update 3225. Maximum Score From Grid Operations.py

--- POTD/DP/3D/Python /3225. Maximum Score From Grid Operations.py
+++ POTD/DP/3D/Python /3225. Maximum Score From Grid Operations.py
@@ -1,40 +1,37 @@
+
+
 class Solution:
     def maximumScore(self, grid: List[List[int]]) -> int:
-        n=len(grid)
-        colPrefSum=[[0]*(n+1) for _ in range(n+1)]
+        n = len(grid)
+        colPrefSum = [[0] * (n + 1) for _ in range(n + 1)]
 
-        for col in range(1,n+1):
-            for row in range(1,n+1):
-                colPrefSum[row][col]=colPrefSum[row-1][col]+grid[row-1][col-1]
+        for col in range(1, n + 1):
+            for row in range(1, n + 1):
+                colPrefSum[row][col] = colPrefSum[row - 1][col] + grid[row - 1][col - 1]
         
-        self.memo = [[[-1] * 2 for _ in range(102)] for _ in range(102)]
-        return self.solve(-1,0,False,grid,colPrefSum,n)
+        @cache
+        def solve(prev_h: int, col: int, prev_taken: bool) -> int:
+            if col >= n:
+                return 0
 
-    
-    def solve(self,prev_h:int,col:int,prev_taken:bool,grid:List[List[int]],colPrefSum:List[List[int]],n:int):
-        if col>=n:
-            return 0
+            result = 0
 
-        if self.memo[prev_h + 1][col][prev_taken]!=-1:
-            return self.memo[prev_h + 1][col][prev_taken]
-        result=0
+            for height in range(-1, n):
+                cur_h = height
+                prevScore = 0
+                curScore = 0
 
-        for height in range(-1,n):
-            cur_h=height
-            prevScore=0
-            curScore=0
+                if prev_h > cur_h:
+                    curScore = colPrefSum[prev_h + 1][col + 1] - colPrefSum[cur_h + 1][col + 1]
+                
+                if not prev_taken and cur_h > prev_h:
+                    prevScore = colPrefSum[cur_h + 1][col] - colPrefSum[prev_h + 1][col]
+                
+                curTaken = curScore + prevScore + solve(cur_h, col + 1, True)
+                curNotTaken = prevScore + solve(cur_h, col + 1, False)
 
-            if prev_h>cur_h:
-                curScore=colPrefSum[prev_h+1][col+1]-colPrefSum[cur_h+1][col+1]
+                result = max(result, curTaken, curNotTaken)
             
-            if not prev_taken and cur_h>prev_h:
-                prevScore=colPrefSum[cur_h+1][col]-colPrefSum[prev_h+1][col]
-            
-            curTaken=curScore+prevScore + self.solve(cur_h,col+1,True,grid,colPrefSum,n)
-            curNotTaken=prevScore + self.solve(cur_h,col+1,False,grid,colPrefSum,n)
+            return result
 
-            result = max(result, curTaken, curNotTaken)
-        
-        self.memo[prev_h + 1][col][prev_taken]=result
-        return self.memo[prev_h + 1][col][prev_taken]
-
+        return solve(-1, 0, False)
