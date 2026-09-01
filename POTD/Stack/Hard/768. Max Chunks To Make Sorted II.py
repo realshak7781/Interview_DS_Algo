0f0b9a41bcfa1e03093196efd24dfb3b3e44ecaa
@sharique8 <124501803+realshak7781@users.noreply.github.com>
2026-09-01 16:52:01 +0530

    Update 768. Max Chunks To Make Sorted II.py

--- POTD/Stack/Hard/768. Max Chunks To Make Sorted II.py
+++ POTD/Stack/Hard/768. Max Chunks To Make Sorted II.py
@@ -1,3 +1,29 @@
+# APPROACH : USING SUFFIX MIN AND PREFIX MAX
+# TIME : O(N)
+# SPACE : O(N)
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n=len(arr)
+        suffixMin=[0]*n
+
+        suffixMin[-1]=arr[-1]
+
+        for i in range(n-2,-1,-1):
+            suffixMin[i]=min(suffixMin[i+1],arr[i])
+        
+
+        chunks=0
+        prefixMax=float('-inf')
+
+        for i in range(n-1):
+            prefixMax=max(prefixMax,arr[i])
+
+            if prefixMax<=suffixMin[i+1]:
+                chunks+=1
+        
+        return chunks+1
+
+
 # TIME : O(N^3)
 # SPACE : O(N*N)
 # APPROACH 1: USING CACHING RECURSION
